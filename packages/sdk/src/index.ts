ff514ab38f8deb5b20ff1702d591692aef3069e8
nioni <stivennoni77@gmail.com>
2022-11-17 17:39:46 +0200

    add cache.ts into common

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,21 @@
-import { Manager } from "./manager";
+import { Cache, Request, Response } from "rpch-commons";
+import RequestCache from "./request-cache";
 
-console.log("we got a manager!", !!Manager);
+// export default class SDK {
+//   private cache: Cache
+//   private requestCache: RequestCache
+
+//   constructor() {
+//     this.cache = new Cache
+//   }
+
+//   public createRequest(
+//     origin: string,
+//     provider: string,
+//     destination: string
+//   ): Request {
+//     return Request.fromData(origin, provider, destination);
+//   }
+
+//   public sendRequest(req: Request): Promise<Response> {}
+// }
