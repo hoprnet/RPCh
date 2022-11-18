d68e894ca445b456623e5b82d0d1420bc3b4dac6
Diego Alzate <diego35.da@gmail.com>
2022-11-18 17:14:05 -0500

    rpch sdk hoprd module and initial part of request cache

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,21 +1,58 @@
-import { Cache, Request, Response } from "rpch-commons";
+import { Cache, Request, Response, Segment } from "rpch-commons";
 import RequestCache from "./request-cache";
+import { sendMessage, createMessageListener } from "./hoprd";
 
-// export default class SDK {
-//   private cache: Cache
-//   private requestCache: RequestCache
+const MOCK_DISCOVERY_PLATFORM_API_ENDPOINT = "https://localhost:3000";
+const MOCK_API_TOKEN = "123456789";
+const MOCK_DESTINATION =
+  "16Uiu2HAmM9KAPaXA4eAz58Q7Eb3LEkDvLarU4utkyL6vM5mwDeEK";
+const TIMEOUT = 60e3;
+export default class SDK {
+  private cache: Cache;
+  private requestCache: RequestCache;
 
-//   constructor() {
-//     this.cache = new Cache
-//   }
+  constructor() {
+    this.requestCache = new RequestCache(TIMEOUT);
+    this.cache = new Cache(
+      TIMEOUT,
+      this.requestCache.onRequestFromSegments,
+      this.requestCache.onResponseFromSegments
+    );
+  }
 
-//   public createRequest(
-//     origin: string,
-//     provider: string,
-//     destination: string
-//   ): Request {
-//     return Request.fromData(origin, provider, destination);
-//   }
+  public requestMessagingAccessToken(apiEndpoint: string, apiToken: string) {
+    // get api token access
+    // open listener
+    createMessageListener(
+      MOCK_DISCOVERY_PLATFORM_API_ENDPOINT,
+      MOCK_API_TOKEN,
+      (message) => {
+        // send data to cache
+      }
+    );
+  }
 
-//   public sendRequest(req: Request): Promise<Response> {}
-// }
+  public createRequest(
+    origin: string,
+    provider: string,
+    destination: string
+  ): Request {
+    return Request.fromData(origin, provider, destination);
+  }
+
+  public sendRequest(req: Request): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      const message = req.toMessage();
+      const segments = message.toSegments();
+      this.requestCache.addRequest(req, resolve, reject);
+      for (const segment of segments) {
+        sendMessage({
+          apiEndpoint: MOCK_DISCOVERY_PLATFORM_API_ENDPOINT,
+          apiToken: MOCK_API_TOKEN,
+          message: segment.toString(),
+          destination: MOCK_DESTINATION,
+        });
+      }
+    });
+  }
+}
--- packages/sdk/src/request-cache.ts
+++ packages/sdk/src/request-cache.ts
@@ -1,191 +1,38 @@
 import { Cache as SegmentsCache, Request, Response, utils } from "rpch-commons";
 
-const { log, logVerbose, logError } = utils.createLogger("cache");
+const { log, logVerbose, logError } = utils.createLogger("request-cache");
 
-export default class Cache {
-  private segmentsCache: SegmentsCache;
+export default class RequestCache {
   // requests we have made to another relay, keyed by message.id
   private requests = new Map<
     number,
     {
       request: Request;
       createdAt: Date;
+      resolve: (value: Response | PromiseLike<Response>) => void;
+      reject: (reason?: any) => void;
     }
   >();
 
-  constructor(private timeout: number) {
-    this.segmentsCache = new SegmentsCache(
-      this.timeout,
-      this.onRequestFromSegments,
-      this.onResponseFromSegments
-    );
-  }
+  constructor(private timeout: number) {}
 
-  public addRequest(req: Request): void {
+  public addRequest(
+    req: Request,
+    resolve: (value: Response | PromiseLike<Response>) => void,
+    reject: (reason?: any) => void
+  ): void {
     this.requests.set(req.id, {
       request: req,
       createdAt: new Date(),
+      resolve,
+      reject,
     });
   }
 
-  private onRequestFromSegments(req: Request): void {}
+  public removeRequest(req: Request): void {
+    this.requests.delete(req.id);
+  }
 
-  private onResponseFromSegments(res: Response): void {}
+  public onRequestFromSegments(req: Request): void {}
+  public onResponseFromSegments(res: Response): void {}
 }
-
-// let reqCreatedCount = 0;
-// let resReceivedCount = 0;
-
-// /**
-//  * Stores requests made and incoming segments.
-//  */
-// export class Manager {
-//   // requests we have made to another relay, keyed by message.id
-//   private requests = new Map<
-//     number,
-//     {
-//       request: Request;
-//       responseObj: ServerResponse;
-//       createdAt: Date;
-//     }
-//   >();
-
-//   // partial segments received, keyed by segment.msgId
-//   private segments = new Map<
-//     number,
-//     {
-//       segments: Segment[];
-//       receivedAt: Date;
-//     }
-//   >();
-
-//   constructor(
-//     private timeout: number,
-//     private sendMessageToHOPRd: (
-//       segment: Segment,
-//       destination: string
-//     ) => Promise<void>,
-//     private sendRequestToProvider: (
-//       request: Request,
-//       provider: string
-//     ) => Promise<string>
-//   ) {}
-
-//   /**
-//    * Break apart message to segments and sent it
-//    * over the HOPR network.
-//    * @param message
-//    * @param destination
-//    */
-//   private async sendMessage(
-//     message: Message,
-//     destination: string
-//   ): Promise<void> {
-//     for (const segment of message.toSegments()) {
-//       await this.sendMessageToHOPRd(segment, destination);
-//     }
-//   }
-
-//   /**
-//    * Create a reqeust and send it over
-//    * the HOPR network.
-//    * @param request
-//    * @param responseObj http response object
-//    * @param destination exit peer id
-//    */
-//   public async createRequest(
-//     request: Request,
-//     responseObj: ServerResponse,
-//     destination: string
-//   ): Promise<void> {
-//     logVerbose("requests created", ++reqCreatedCount);
-//     this.requests.set(request.id, {
-//       request,
-//       createdAt: new Date(),
-//       responseObj,
-//     });
-//     await this.sendMessage(request.toMessage(), destination);
-//   }
-
-//   public handleReceivedMessageResponse(response: Response): void {
-//     const requestEntry = this.requests.get(response.id);
-//     if (!requestEntry) {
-//       logError("matching request not found", response.id);
-//       return;
-//     }
-
-//     requestEntry.responseObj.write(response.body);
-//     requestEntry.responseObj.statusCode = 200;
-//     requestEntry.responseObj.end();
-//     this.requests.delete(response.id);
-//     logVerbose("responses received", ++resReceivedCount);
-//     log("responded to %s with %s", requestEntry.request.body, response.body);
-//   }
-
-//   public async handleReceivedMessageRequest(request: Request): Promise<void> {
-//     const response = await this.sendRequestToProvider(
-//       request,
-//       request.provider
-//     );
-//     await this.sendMessage(
-//       request.createResponse(response).toMessage(),
-//       request.origin
-//     );
-//   }
-
-//   public onSegmentReceived(segment: Segment): void {
-//     // get segment entry with matching msgId, or create a new one
-//     const segmentEntry = this.segments.get(segment.msgId) || {
-//       segments: [] as Segment[],
-//       receivedAt: new Date(),
-//     };
-
-//     if (segmentEntry.segments.find((s) => s.segmentNr === segment.segmentNr)) {
-//       log("dropping segment, already exists", segment.msgId, segment.segmentNr);
-//       return;
-//     }
-
-//     segmentEntry.segments = [...segmentEntry.segments, segment];
-//     this.segments.set(segment.msgId, segmentEntry);
-//     log("stored new segment");
-
-//     if (validateSegments(segmentEntry.segments)) {
-//       const message = Message.fromSegments(segmentEntry.segments);
-
-//       try {
-//         const req = Request.fromMessage(message);
-//         this.handleReceivedMessageRequest(req);
-//       } catch {
-//         const res = Response.fromMessage(message);
-//         this.handleReceivedMessageResponse(res);
-//       }
-
-//       // remove segments
-//       this.segments.delete(segment.msgId);
-//     }
-//   }
-
-//   public removeExpired(): void {
-//     const now = new Date();
-
-//     logVerbose("requests", this.requests.size);
-//     logVerbose("segments", this.segments.size);
-
-//     for (const [id, entry] of this.requests.entries()) {
-//       log(utils.isExpired(this.timeout, now, entry.createdAt));
-//       if (utils.isExpired(this.timeout, now, entry.createdAt)) {
-//         log("dropping expired request");
-//         this.requests.delete(id);
-//         entry.responseObj.statusCode = 400;
-//         entry.responseObj.end();
-//       }
-//     }
-
-//     for (const [id, entry] of this.segments.entries()) {
-//       if (utils.isExpired(this.timeout, now, entry.receivedAt)) {
-//         log("dropping expired partial segments");
-//         this.segments.delete(id);
-//       }
-//     }
-//   }
-// }
