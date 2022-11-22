33ad9be8a43eb2a95b9343bdb3d6a4dca64ad36b
camilovega <camilovegag@unisabana.edu.co>
2022-11-21 22:13:11 -0500

    Add onResponse test and start removeExpired test

--- packages/common/src/cache.spec.ts
+++ packages/common/src/cache.spec.ts
@@ -1,30 +1,51 @@
-import Cache from "./cache";
 import assert from "assert";
-import { RPC_REQ_LARGE } from "./fixtures";
+import Cache from "./cache";
+import { PEER_ID_A, PROVIDER, RPC_REQ_LARGE } from "./fixtures";
 import Request from "./request";
+import Response from "./response";
 
-const fakeRequest = new Request(1, "node", "prov", RPC_REQ_LARGE);
-const fakeSegmets = fakeRequest.toMessage().toSegments();
+const ID = 828161;
+const RESPONSE = "response";
 
-// test onRequest
+const fakeRequest = new Request(ID, PEER_ID_A, PROVIDER, RPC_REQ_LARGE);
+const fakeRequestSegments = fakeRequest.toMessage().toSegments();
+
+const fakeResponse = new Response(ID, RESPONSE);
+const fakeResponseSegments = fakeResponse.toMessage().toSegments();
+
+// why anonymous function?
 describe("test Cache class", function () {
-  it("should reconstruct message", (done) => {
-    const cache = new Cache(
-      60e3,
-      (request) => {
-        assert.equal(request.toMessage().body, "hello");
-        done();
-      },
-      console.log
-    );
-    for (const segment of fakeSegmets) {
+  it("should reconstruct request message", () => {
+    const cache = new Cache(60e3, (request) => {
+      assert.equal(request.toMessage().body, fakeRequest.toMessage().body);
+    }, console.log);
+    for (const segment of fakeRequestSegments) {
       cache.onSegment(segment);
     }
-  });
-});
+  })
+  it("should reconstruct response message", () => {
+    const cache = new Cache(60e3, console.log, (response) => {
+      assert.equal(response.body, RESPONSE);
+    });
+    for (const segment of fakeResponseSegments) {
+      cache.onSegment(segment);
+    }
+  })
+  it("should drop duplicate segments", () => {
+    const cache = new Cache(60e3, () => { }, () => { });
+    // @ts-ignore-next-line
+    const segments = cache.segments;
+    console.log('@segments: ', segments);
 
-// test onResponse
+    for (const segment of fakeRequestSegments) {
+      cache.onSegment(segment);
+    }
 
-// test onSegment
+    const currentSegment = segments.get(828161);
+    segments.set(828161, { segments: [...currentSegment!.segments], receivedAt: new Date(1669083639349) });
 
-// test removeExpired
+    cache.removeExpired();
+
+  })
+})
+
