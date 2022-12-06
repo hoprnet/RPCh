6a2d03212b8ad6a276c0b8f186694699d1558988
nioni <stivennoni77@gmail.com>
2022-12-06 14:39:30 +0200

    remove unused code

--- packages/crypto/src/index.spec.ts
+++ packages/crypto/src/index.spec.ts
@@ -13,16 +13,10 @@ import {
 
 set_panic_hook();
 
-const PRIV_KEY_ENTRY_NODE = utils.arrayify(
-  "0xd12c951563ee7e322562b7ce7a31c37cc6c10d9b86f834ed30f7c4ab42ae8de0"
-);
 const PRIV_KEY_EXIT_NODE = utils.arrayify(
   "0x1a7a8c37e30c97ebf532042bdc37fe724a3950b0cd7ea5a57c9f3e30c53c44a3"
 );
 
-const PUB_KEY_ENTRY_NODE = utils.arrayify(
-  "0x02d9c0e0ab99d251a8fd2cd48df6554dfd5112afe589a3dcab75928aea34f98581"
-);
 const PUB_KEY_EXIT_NODE = utils.arrayify(
   "0x021be92a59234dbef617f5eb0d5426758a6cad16f951458a3d753aa22c09e75509"
 );
@@ -34,11 +28,6 @@ const PEER_ID_EXIT_NODE = PeerId.createFromB58String(
   "16Uiu2HAkwJdCap1ErGKjtLeHjfnN53TD8kryG48NYVPWx4HhRfKW"
 );
 
-const IDENTITY_ENTRY_NODE = Identity.load_identity(
-  PUB_KEY_ENTRY_NODE,
-  PRIV_KEY_ENTRY_NODE,
-  BigInt(0)
-);
 const IDENTITY_EXIT_NODE = Identity.load_identity(
   PUB_KEY_EXIT_NODE,
   PRIV_KEY_EXIT_NODE,
@@ -69,9 +58,10 @@ describe("test index.ts", function () {
     exitNodeCounter = session_client.counter();
 
     // exit node
+    const encrypted_req_data_received = session_client.get_request_data();
     const session_exit_node = unbox_request(
       new Envelope(
-        session_client.get_request_data(),
+        encrypted_req_data_received,
         PEER_ID_ENTRY_NODE.toB58String(),
         PEER_ID_EXIT_NODE.toB58String()
       ),
@@ -99,10 +89,11 @@ describe("test index.ts", function () {
     clientCounter = session_exit_node.counter();
 
     // client
+    const encrypted_res_data_received = session_exit_node.get_response_data();
     unbox_response(
       session_client,
       new Envelope(
-        session_exit_node.get_response_data(),
+        encrypted_res_data_received,
         PEER_ID_ENTRY_NODE.toB58String(),
         PEER_ID_EXIT_NODE.toB58String()
       )
