5792df89f13b67305dcb88fb0c7835f888461695
nioni <stivennoni77@gmail.com>
2022-12-21 22:10:27 +0200

    add start to turbo.json

--- devkit/e2e/e2e.sh
+++ devkit/e2e/e2e.sh
@@ -81,7 +81,7 @@ exit_node_peer_id=$(cat logs |grep "node5" -A 1 | grep "Peer Id" | awk '{ print 
 
 # Run tests with env variables
 ENTRY_NODE_PEER_ID="$entry_node_peer_id" EXIT_NODE_PEER_ID="$exit_node_peer_id" ENTRY_NODE_API_TOKEN="$HOPRD_API_TOKEN" \
-yarn test-e2e
+npx jest --coverage
 
 # After tests exit tear down setup
 docker compose -f ../sandbox/docker-compose.yml down
