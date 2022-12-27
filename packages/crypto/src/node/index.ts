c2d600381d5cbf4bc0bf16f79fcd1ad80da0be87
nioni <stivennoni77@gmail.com>
2022-12-27 11:54:21 +0200

    remove unused code

--- packages/crypto/src/node/index.ts
+++
@@ -1,7 +0,0 @@
-import { set_panic_hook } from "@rpch/crypto/nodejs";
-// @ts-ignore
-globalThis.crypto = require("node:crypto").webcrypto;
-
-set_panic_hook();
-
-export * from "@rpch/crypto/nodejs";
