ce22626cd89b8bac46bb1b5d847b9e6ead272cf9
nioni <stivennoni77@gmail.com>
2022-12-27 11:45:01 +0200

    ue old school multi-entrypoint setup

--- packages/crypto/tsup.config.js
+++ packages/crypto/tsup.config.js
@@ -3,5 +3,9 @@ const baseConfig = require("rpch-configs-tsup");
 /** @type {import('tsup').Options} */
 module.exports = {
   ...baseConfig,
+  // if this is true, it will remove all files in our directory
+  // we use rimraf instead
+  clean: false,
+  outDir: "./",
   target: "es5",
 };
