1c554a7e703f4d5a4a4e3b3be3f6301fb990c5aa
Luis Martinez <luisemartinezcruz@gmail.com>
2022-12-28 00:23:13 -0500

    Fix Docker image for M1 chip

--- apps/exit-node/src/index.ts
+++ apps/exit-node/src/index.ts
@@ -105,8 +105,9 @@ export const start = async (ops: {
     password: ops.password,
     privateKey: ops.privateKey,
   });
+  console.log();
+  log.verbose("Running exit node with public key", publicKey);
   log.verbose("Got identity");
-  log.normal("Running exit node with public key", publicKey);
 
   const cache = new Cache(onMessage);
   const interval: NodeJS.Timer = setInterval(() => {
--- devkit/e2e/e2e.sh
+++ devkit/e2e/e2e.sh
@@ -76,13 +76,17 @@ done
 echo "Done!"
 
 # Extract entry and exit node peer ids from pluto logs
-entry_node_peer_id=$(cat logs |grep "node1" -A 1 | grep "Peer Id" | awk '{ print $5 }')
-exit_node_peer_id=$(cat logs |grep "node5" -A 1 | grep "Peer Id" | awk '{ print $5 }')
+entry_node_peer_id=$(cat logs | grep "node1" -A 1 | grep "Peer Id" | awk '{ print $5 }')
+exit_node_peer_id=$(cat logs | grep "node5" -A 1 | grep "Peer Id" | awk '{ print $5 }')
+exit_node_pub_key=$(echo "$logs5" | grep "Running exit node with public key" | awk '{print $9}')
 
+echo "Right before running tests"
+echo "EXIT NODE PUB KEY: $exit_node_pub_key"
 # Run tests with env variables
 ENTRY_NODE_PEER_ID="$entry_node_peer_id" EXIT_NODE_PEER_ID="$exit_node_peer_id" ENTRY_NODE_API_TOKEN="$HOPRD_API_TOKEN" \
-npx jest --coverage
+EXIT_NODE_PUB_KEY="$exit_node_pub_key" npx jest --coverage
 
+echo "Tearing down setup"
 # After tests exit tear down setup
 docker compose -f ../sandbox/docker-compose.yml down
 rm ./logs
