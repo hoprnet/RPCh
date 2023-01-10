4d27602984256943225bc1944000089ce9ac4413
Luis Martinez <luisemartinezcruz@gmail.com>
2023-01-10 11:20:00 -0500

    Delete unnecesary echo

--- devkit/sandbox/src/common.sh
+++ devkit/sandbox/src/common.sh
@@ -5,7 +5,6 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
 # stop sandbox
 stop() {
-    echo "$DIR"
     docker-compose -p sandbox-nodes down;
     docker-compose -p sandbox-central down;
     exit;
