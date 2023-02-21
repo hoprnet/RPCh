48d7020e7ec4fdad353f527a95537a374e25484b
Diego Alzate <diego35.da@gmail.com>
2023-02-20 20:32:17 -0500

    remove not null constraint for

--- apps/discovery-platform/src/migrations/1676923329450_add-paid-by-column.ts
+++ apps/discovery-platform/src/migrations/1676923329450_add-paid-by-column.ts
@@ -2,7 +2,7 @@ import { MigrationBuilder } from "node-pg-migrate";
 
 export const up = async (pgm: MigrationBuilder) => {
   await pgm.addColumn("quotas", {
-    paid_by: { type: "varchar(255)", notNull: true, references: "clients(id)" },
+    paid_by: { type: "varchar(255)", references: "clients(id)" },
   });
 };
 
