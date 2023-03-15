bae84d6f945d7e73b531e3ec4ed87d2dde4889af
Robert Kiel <robert.kiel@hoprnet.org>
2023-03-15 12:15:52 +0100

    fix utf8 characters

--- packages/common/src/utils/index.spec.ts
+++ packages/common/src/utils/index.spec.ts
@@ -6,14 +6,30 @@ import { createMockedFlow, RPC_REQ_LARGE } from "../fixtures";
 describe("test utils / splitStrByBytes", function () {
   it("should return 1 string", function () {
     const str = "helloworld";
-    assert.equal(utils.toUtf8Bytes(str).byteLength, 10);
-    assert.equal(splitStrByBytes(str, 10).length, 1);
+
+    const res = splitStrByBytes(str, 10);
+    const expected = [ "helloworld" ];
+
+    assert(res.every((str, i) => expected[i] === str))
   });
   it("should return 3 strings", function () {
     const str = "helloworldhelloworldhelloworld";
+
     assert.equal(utils.toUtf8Bytes(str).byteLength, 30);
-    assert.equal(splitStrByBytes(str, 10).length, 3);
+
+    const res = splitStrByBytes(str, 10);
+    const expected = ["helloworld", "helloworld", "helloworld"];
+
+    assert(res.every((str, i) => expected[i] === str))
   });
+  it("should respect Utf-8 character sizes", function () {
+    const str = "A𝑨B𝑩C𝑪";
+
+    const res = splitStrByBytes(str, 2);
+    const expected = [ 'A', '𝑨', 'B', '𝑩', 'C', '𝑪' ];
+
+    assert(res.every((str, i) => expected[i] === str))
+  })
 });
 
 describe("test utils / isExpired", function () {
@@ -21,7 +37,6 @@ describe("test utils / isExpired", function () {
   const timeout = 10e3;
   const inFiveSecs = new Date(createdAt.valueOf() + 5e3);
   const inTenSecs = new Date(createdAt.valueOf() + 15e3);
-
   it("should return false after 5 seconds", function () {
     assert(!isExpired(timeout, inFiveSecs, createdAt));
   });
