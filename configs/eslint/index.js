5b1cb997f583a7b69a11aef5255df251836c2503
Diego Alzate <diego35.da@gmail.com>
2023-03-29 07:51:38 -0500

    remove next from eslint as we do not have any front end project

--- configs/eslint/index.js
+++ configs/eslint/index.js
@@ -8,12 +8,10 @@
  * @type {EslintConfig}
  */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier"],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
   },
