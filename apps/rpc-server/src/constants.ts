31c032c5690c42785d09572893068a3e04cf0b41
Diego Alzate <diego35.da@gmail.com>
2023-04-12 14:44:11 -0500

    fix format

--- apps/rpc-server/src/constants.ts
+++ apps/rpc-server/src/constants.ts
@@ -1,17 +1,16 @@
 import * as path from "path";
-require('dotenv').config({path:path.join(process.cwd(), './.env.local')})
+require("dotenv").config({ path: path.join(process.cwd(), "./.env.local") });
 
-const {
-  DISCOVERY_PLATFORM_API_ENDPOINT,
-  CLIENT,
-} = process.env;
+const { DISCOVERY_PLATFORM_API_ENDPOINT, CLIENT } = process.env;
 
-const DATA_DIR = process.env.DATA_DIR ? process.env.DATA_DIR : path.join(process.cwd(), "db");
+const DATA_DIR = process.env.DATA_DIR
+  ? process.env.DATA_DIR
+  : path.join(process.cwd(), "db");
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3040;
 const RESPONSE_TIMEOUT = process.env.RESPONSE_TIMEOUT
   ? Number(process.env.RESPONSE_TIMEOUT)
   : 10000;
-  
+
 export {
   DATA_DIR,
   PORT,
