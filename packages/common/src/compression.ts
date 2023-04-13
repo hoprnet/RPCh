7bdf89e95ecb72ab7795232a9dc2f6b86c3ad29f
Michał Jadach <sarah.smith@email.com>
2023-04-13 18:31:43 +0200

    Compress 'result'{} keys, add vsc debug launcher

--- packages/common/src/compression.ts
+++ packages/common/src/compression.ts
@@ -4,6 +4,8 @@ import {
   Dictionary
 } from "./types";
 
+import * as utils from './utils'
+
 /**
  * Dictionaries used to compress and decompress RPC keys and methods 
  * more info: https://ethereum.org/en/developers/docs/apis/json-rpc/
@@ -28,9 +30,10 @@ const errorKeyMap : Dictionary = {
   3: 'error',
   4: 'at',
   5: 'text',
+  6: 'data',
 }
 
-const resultKeyMap : Dictionary= { //params: [ too
+const resultOrParamsKeyMap : Dictionary= {
   0: 'hash',
   1: 'from',
   2: 'to',
@@ -74,15 +77,15 @@ const resultKeyMap : Dictionary= { //params: [ too
   40:'startingBlock',
   41:'currentBlock',
   42:'highestBlock',
-  43:'blockHash',
-  45:'blockNumber',
+  43:'logIndex',
+  45:'removed',
   46:'contractAddress',
   47:'cumulativeGasUsed',
   48:'effectiveGasPrice',
   49:'from',
   50:'gasUsed',
   51:'logs',
-  52:'logsBloom',
+  52:'address',
   53:'status',
   54:'to',
   55:'transactionHash',
@@ -90,6 +93,39 @@ const resultKeyMap : Dictionary= { //params: [ too
   58:'type',
   59:'code',
   60:'info',
+  61:'maxPriorityFeePerGas',
+  62:'maxFeePerGas',
+  63:'data',
+  64:'author',
+  65:'difficulty',
+  66:'extraData',
+  67:'gasLimit',
+  68:'gasUsed',
+  69:'hash',
+  71:'miner',
+  72:'mixHash',
+  73:'nonce',
+  74:'number',
+  75:'parentHash',
+  76:'receiptsRoot',
+  77:'sha3Uncles',
+  78:'size',
+  79:'stateRoot',
+  80:'totalDifficulty',
+  81:'timestamp',
+  82:'baseFeePerGas',
+  83:'transactions',
+  84:'transactionsRoot',
+  85:'uncles',
+  86:'gasUsedRatio',
+  87:'oldestBlock',
+  88:'reward',
+  89:'accountProof',
+  90:'balance',
+  91:'codeHash',
+  92:'nonce',
+  93:'storageHash',
+  94:'storageProof',
 }
 
 const methodValueMap : Dictionary = {
@@ -164,9 +200,108 @@ const methodValueMap : Dictionary = {
  * 0 no.0 - the content is zipped
  * 0 no.1 - 
  * 0 no.2 - 
- * 0 no.3 - 
+ * 0 no.3 - 'result'{} keys compressed
  * 0 no.4 - 'method' value compressed
  */
+
+const a0 = {
+  "jsonrpc": "2.0",
+  "id": 1,
+  "result": [
+      {
+          "address": "0x1a9469a12afbc91ca1",
+          "blockHash": "0x7c5a35e9cb3ffcab52c70",
+          "blockNumber": "09fb",
+          "data": "0x000000002a23",
+          "logIndex": "0x1d",
+          "removed": false,
+          "topics": [
+              "0x241ea0350f73818b80"
+          ],
+          "transactionHash": "0x3dc91b98fb3063d9c04d50",
+          "transactionIndex": "0x1d"
+      },
+      {
+          "address": "0x06012c87f8e7a266d",
+          "blockHash": "0x7c5a35e9cb3e8ae0edcffcab52c70",
+          "blockNumber": "0x5c29fb",
+          "data": "0x00008e374400000000000000000000000000000000a0f",
+          "logIndex": "0x57",
+          "removed": false,
+          "topics": [
+              "0x241ea03ca8b80"
+          ],
+          "transactionHash": "0x788b14b34e92beec1",
+          "transactionIndex": "0x54"
+      }
+  ]
+};
+
+const a1 = {
+  "jsonrpc": "2.0",
+  "result": {
+      "author": "0x0000000000000000000",
+      "difficulty": "0x0",
+      "extraData": "0x4e6696e64",
+      "5": "0x103a718",
+      "gasUsed": "0xe03a3",
+      "hash": "0x9a26373f41e528a5f635467c592ea353e09680f3c9b2f521aaa5e349bca948aa",
+      "logsBloom": "0x00000000000000000000000000000000000000000000008400000000000000002000080000000000200120000000000000000000000008000000000000000000080000000000000004000000000000000000000000000000000000000000000000008000000010000000000040000000000000000000000000000200000000000000100000004000000000020000080000004000000000000000000000000000000000000000000000000000081002000000000000000000000000000000000000200000000000000040000010002000000000000000000008000000000000040000000000000000000402",
+      "miner": "0x0000000000000000000000000",
+      "mixHash": "0x2e803d00e46c7c4f235bfdf882",
+      "nonce": "0x0000000000000000",
+      "55": "0x1a2801d",
+      "parentHash": "0xa2f774501c0bc3ee",
+      "receiptsRoot": "0xca4b47252fcb9e",
+      "sha3Uncles": "0x1dcc4de0d49347",
+      "size": "0x7db",
+      "stateRoot": "0x251fdd08acd0c4fb5dec3f5273db2589c9b8",
+      "totalDifficulty": "0x2",
+      "timestamp": "0x6437f1c2",
+      "baseFeePerGas": "0x7",
+      "transactions": [
+          "0xf81b4659bcde2e005501473b994",
+          "0x5f0d6a7afc5f273eb0d",
+          "0x1b115daa7f38c2"
+      ],
+      "transactionsRoot": "0x52aeed76a9060fed",
+      "uncles": []
+  },
+  "id": 73312
+};
+
+const a2 = {
+  "jsonrpc": "2.0",
+  "result": {
+      "author": "0x0000000000000000000",
+      "difficulty": "0x0",
+      "extraData": "0x4e6696e64",
+      "5": "0x103a718",
+      "gasUsed": "0xe03a3",
+      "hash": "0x9a26373f41e528a5f635467c592ea353e09680f3c9b2f521aaa5e349bca948aa",
+      "logsBloom": "0x00000000000000000000000000000000000000000000008400000000000000002000080000000000200120000000000000000000000008000000000000000000080000000000000004000000000000000000000000000000000000000000000000008000000010000000000040000000000000000000000000000200000000000000100000004000000000020000080000004000000000000000000000000000000000000000000000000000081002000000000000000000000000000000000000200000000000000040000010002000000000000000000008000000000000040000000000000000000402",
+      "miner": "0x0000000000000000000000000",
+      "mixHash": "0x2e803d00e46c7c4f235bfdf882",
+      "nonce": "0x0000000000000000",
+      "number": "0x1a2801d",
+      "parentHash": "0xa2f774501c0bc3ee",
+      "receiptsRoot": "0xca4b47252fcb9e",
+      "sha3Uncles": "0x1dcc4de0d49347",
+      "size": "0x7db",
+      "stateRoot": "0x251fdd08acd0c4fb5dec3f5273db2589c9b8",
+      "totalDifficulty": "0x2",
+      "timestamp": "0x6437f1c2",
+      "baseFeePerGas": "0x7",
+      "transactions": [
+          "0xf81b4659bcde2e005501473b994",
+          "0x5f0d6a7afc5f273eb0d",
+          "0x1b115daa7f38c2"
+      ],
+      "transactionsRoot": "0x52aeed76a9060fed",
+      "uncles": []
+  },
+  "id": 73312
+};
 
 export default class Compression {
 
@@ -177,27 +312,34 @@ export default class Compression {
     const jsonKeys : string[] = Object.keys(requestBody);
 
     //Compress 'method' Value
-    let result = Compression.compressedRPCMethodValue(jsonTmp);
+    let result = Compression.compressRPCMethodValue(jsonTmp);
     if (result.compressed) {
-      Compression.replaceAt(compressionDiagram, 4, '1');
+      // @ts-ignore-start
+      compressionDiagram = utils.replaceInStringAt(compressionDiagram, 4, '1');
+      // @ts-ignore-end
       jsonTmp = result.json;
     }
 
-    return clientResponse;
+    //Compress 'result'{} keys
+    result = Compression.compressRPCSomeObjectKeys(jsonTmp, 'result');
+    if (result.compressed) {
+      // @ts-ignore-start
+      compressionDiagram = utils.replaceInStringAt(compressionDiagram, 3, '1');
+      // @ts-ignore-end
+      jsonTmp = result.json;
+    }
+    
+    return jsonTmp;
   }
 
 
-  private static replaceAt(string: string, index: number, replacement: string) {
-    return string.substring(0, index) + replacement + string.substring(index + replacement.length);
-  }
-
-  private static getCompressedKeyId(key: string, dictionary: Dictionary): string | null {
+  private static getCompressedKeyId(key: string, dictionary: Dictionary): string | null | PropertyKey {
     let id = null;
     const dictionaryKeys : string[] = Object.keys(dictionary);
 
     for(let i = 0; i < dictionaryKeys.length; i++) {
-      if(key === dictionaryKeys[i]) {
-        id = key;
+      if(key === dictionary[dictionaryKeys[i]]) {
+        id = dictionaryKeys[i];
         break;
       }
     }
@@ -205,15 +347,13 @@ export default class Compression {
     return id;
   }
 
-  private static compressedRPCMethodValue(input: JSONObject): JSONObject {
+  private static compressRPCMethodValue(input: JSONObject): JSONObject {
     let result : JSONObject = {
       compressed: false,
       json: JSON.parse(JSON.stringify(input))
     };
-    const inputKeys : string[] = Object.keys(input);
-    const dictionaryKeys : string[] = Object.keys(methodValueMap);
 
-    if(inputKeys.includes('method')) {
+    if(input['method']) {
       const method : string = input['method'];
       const methodId = Compression.getCompressedKeyId(method, methodValueMap);
       result.json['method'] = methodId;
@@ -223,8 +363,75 @@ export default class Compression {
     return result;
   }
 
+  private static compressRPCSomeObjectKeys(input: JSONObject, objectKey: string): JSONObject {
+    let result : JSONObject = {
+      compressed: false,
+      json: JSON.parse(JSON.stringify(input))
+    };
+
+    if(!input[objectKey]) return result;
+
+    const isArray = utils.isArray(input[objectKey]);
+    const isObject = utils.isJsonObject(input[objectKey]);
+    if(!isArray && !isObject) return result;
+
+    const dictionaryKeys : string[] = Object.keys(resultOrParamsKeyMap);
+    const isArrayOfJsonObjects = utils.isArrayOfJsonObjects(input[objectKey]);
+    if(isArrayOfJsonObjects) {
+      //check if new keys do not create conflicts with old keys
+      let cantContinue = false;
+      for (let i = 0; i < input[objectKey].length; i++) {
+        const tmpObjKeys: string[] = Object.keys(input[objectKey][i]);
+        cantContinue = utils.findCommonElement(dictionaryKeys, tmpObjKeys);
+        if (cantContinue) return result;
+      }
+
+      for (let i = 0; i < input[objectKey].length; i++) {
+        let tmpObj = JSON.parse(JSON.stringify(input[objectKey][i]));
+        const tmpObjKeys: string[] = Object.keys(input[objectKey][i]);
+
+        for (let k = 0; k < tmpObjKeys.length; k++) {
+          const oldKey : any = tmpObjKeys[k];
+          const keyId = Compression.getCompressedKeyId(oldKey, resultOrParamsKeyMap);
+          if (keyId) {
+            tmpObj[keyId] = input[objectKey][i][oldKey];
+            delete tmpObj[oldKey];
+            result.compressed = true;
+          }
+        }
+        result.json[objectKey][i] = JSON.parse(JSON.stringify(tmpObj));
+      }
+    } else if(isObject) {
+      //check if new keys do not create conflicts with old keys
+      const tmpObjKeys: string[] = Object.keys(input[objectKey]);
+      const cantContinue = utils.findCommonElement(dictionaryKeys, tmpObjKeys);
+      if (cantContinue) return result;
+
+      let tmpObj = JSON.parse(JSON.stringify(input[objectKey]));
+
+      for (let k = 0; k < tmpObjKeys.length; k++) {
+        const oldKey : any = tmpObjKeys[k];
+        const keyId = Compression.getCompressedKeyId(oldKey, resultOrParamsKeyMap);
+        if (keyId) {
+          tmpObj[keyId] = input[objectKey][oldKey];
+          delete tmpObj[oldKey];
+          result.compressed = true;
+        }
+      }
+
+      result.json[objectKey] = JSON.parse(JSON.stringify(tmpObj));
+    }
+
+
+    
 
     return result;
   }
 
 }
+
+
+//Compression.compressRpcRequest(a0);
+//Compression.compressRpcRequest(a1);
+
+console.log('wait');
--- packages/common/src/utils/index.spec.ts
+++ packages/common/src/utils/index.spec.ts
@@ -1,6 +1,15 @@
 import assert from "assert";
 import { utils } from "ethers";
-import { splitStrByBytes, isExpired, areAllSegmentsPresent } from "../utils";
+import { 
+  splitStrByBytes, 
+  isExpired, 
+  areAllSegmentsPresent,
+  replaceInStringAt,
+  isArray,
+  isJsonObject,
+  isArrayOfJsonObjects,
+  findCommonElement,
+} from "../utils";
 import { createMockedFlow, RPC_REQ_LARGE } from "../fixtures";
 
 describe("test utils / splitStrByBytes", function () {
@@ -42,5 +51,103 @@ describe("test utils / areAllSegmentsPresent", function () {
   });
   it("should return false", function () {
     assert(!areAllSegmentsPresent(incompleteSegments));
+  });
+});
+
+describe("test utils / areAllSegmentsPresent", function () {
+  const completeSegments = createMockedFlow(RPC_REQ_LARGE)
+    .next()
+    .value.toMessage()
+    .toSegments();
+  const incompleteSegments = completeSegments.slice(1);
+
+  it("should return true", function () {
+    assert(areAllSegmentsPresent(completeSegments));
+  });
+  it("should return false", function () {
+    assert(!areAllSegmentsPresent(incompleteSegments));
+  });
+});
+
+describe("test utils / replaceInStringAt", function () {
+  const string = 'abcdef';
+  const string2 = replaceInStringAt(string, 2, 'x');
+
+  it("should return true", function () {
+    assert(string2 === 'abxdef');
+  });
+});
+
+describe("test utils / isArray", function () {
+  const test0 : any = 'abcdef';
+  const test1 : any = [];
+  const test2 : any = {};
+  const test0_res = isArray(test0);
+  const test1_res = isArray(test1);
+  const test2_res = isArray(test2);
+  it("should return true", function () {
+    assert(test1_res);
+  });
+  it("should return false", function () {
+    assert(!(test0_res || test2_res));
+  });
+});
+
+describe("test utils / isJsonObject", function () {
+  const test0 : any = 'abcdef';
+  const test1 : any = [];
+  const test2 : any = {};
+  const test0_res = isJsonObject(test0);
+  const test1_res = isJsonObject(test1);
+  const test2_res = isJsonObject(test2);
+  it("should return true", function () {
+    assert(test2_res);
+  });
+  it("should return false", function () {
+    assert(!(test0_res || test1_res));
+  });
+});
+
+describe("test utils / isArrayOfJsonObjects", function () {
+  const test0 : any = 'abcdef';
+  const test1 : any = [];
+  const test2 : any = {};
+  const test3 : any = {a: 'b', c: 'd'};
+  const test4 : any = {a: {a: 'b', c: 'd'}, c: {a: 'b', c: 'd'}};
+  const test5 : any = ['a', 'b'];
+  const test6 : any = [{a: 'b', c: 'd'}, {a: 'b', c: 'd'}];
+  const test0_res = isArrayOfJsonObjects(test0);
+  const test1_res = isArrayOfJsonObjects(test1);
+  const test2_res = isArrayOfJsonObjects(test2);
+  const test3_res = isArrayOfJsonObjects(test3);
+  const test4_res = isArrayOfJsonObjects(test4);
+  const test5_res = isArrayOfJsonObjects(test5);
+  const test6_res = isArrayOfJsonObjects(test6);
+  it("should return true", function () {
+    assert(test6_res);
+  });
+  it("should return false", function () {
+    assert(!(test0_res || test1_res || test2_res || test3_res || test4_res || test5_res));
+  });
+});
+
+describe("test utils / findCommonElement", function () {
+  const arr1 : string[] = [];
+  const arr2 : string[] = ['a', 'b', 'c', 'dd'];
+  const arr3 : string[] = ['e', 'f', 'g', 'h'];
+  const arr4 : string[] = ['dd', 'x', 'y', 'z'];
+  const test0_res = findCommonElement(arr1, arr1); // false
+  const test1_res = findCommonElement(arr1, arr2); // false
+  const test2_res = findCommonElement(arr1, arr3); // false
+  const test3_res = findCommonElement(arr2, arr4); // true
+  const test4_res = findCommonElement(arr2, arr2); // true
+  const test5_res = findCommonElement(arr2, arr3); // false
+  const test6_res = findCommonElement(arr2, arr4); // true
+  const test7_res = findCommonElement(arr3, arr4); // false
+  it("should return true", function () {
+    assert(test3_res && test4_res && test6_res );
+  });
+  it("should return false", function () {
+    assert(!(test0_res || test1_res || test2_res || test5_res || test7_res));
   });
 });
