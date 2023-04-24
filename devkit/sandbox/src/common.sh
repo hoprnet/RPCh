b062102fee8eb1bea8644ebd007f7adb96274561
Diego Alzate <diego35.da@gmail.com>
2023-04-24 05:40:00 -0500

    fix sandbox script with authorization (#359)
    * fiz sandbox script
    
    * remove uneeded header for sandbox

--- devkit/sandbox/src/common.sh
+++ devkit/sandbox/src/common.sh
@@ -127,9 +127,10 @@ start() {
     echo "Adding quota to 'trial' in 'discovery-platform'"
     curl -X POST "http://127.0.0.1:3030/add-quota" \
         -H "Content-Type: application/json" \
+        -H "x-rpch-client: trial" \
         -d '{
             "discoveryPlatformEndpoint": "'$DISCOVERY_PLATFORM_ENDPOINT'",
-            "X-Rpch-Client": "trial",
+            "client": "trial",
             "quota": "500"
         }'
     echo "Added quota to client 'trial' in 'discovery-platform'"
@@ -177,6 +178,7 @@ start() {
     echo "Adding quota to 'sandbox' in 'discovery-platform'"
     scurl -X POST "http://127.0.0.1:3030/add-quota" \
         -H "Content-Type: application/json" \
+        -H "x-rpch-client: sandbox" \
         -d '{
             "discoveryPlatformEndpoint": "'$DISCOVERY_PLATFORM_ENDPOINT'",
             "client": "sandbox",
