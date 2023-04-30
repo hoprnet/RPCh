59176edb3a549795baa63ea6ad8d3b26b4e88513
Diego Alzate <diego35.da@gmail.com>
2023-04-30 19:48:59 +0300

    remove not used imports

--- packages/common/src/utils/index.spec.ts
+++ packages/common/src/utils/index.spec.ts
@@ -13,8 +13,6 @@ import {
 } from "../utils";
 import { createMockedFlow } from "../fixtures";
 import { req_80kb } from "../compression/compression-samples";
-import http from "http";
-import WS, { WebSocket } from "isomorphic-ws";
 
 describe("test utils / splitStrByBytes", function () {
   it("should return 1 string", function () {
