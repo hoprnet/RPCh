a6163257888fb4e113d929e47b178347c7f92368
nioni <stivennoni77@gmail.com>
2023-06-13 20:38:09 +0300

    hotfix: remove sanity checks

--- packages/common/src/response.ts
+++ packages/common/src/response.ts
@@ -64,9 +64,6 @@ export default class Response {
       counter: bigint
     ) => Promise<void>
   ): Promise<Response> {
-    if (!message.body.startsWith("0x"))
-      throw Error("Message is not a Response");
-
     crypto.unbox_response(
       request.session,
       new crypto.Envelope(
