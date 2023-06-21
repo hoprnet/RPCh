68638de679d6f3269ca85138c358dd5f2156d035
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-06-21 10:14:59 +0200

    initial flake nix setup

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotify-tools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    nodejs-16_x
+    (yarn.override { nodejs = nodejs-16_x; })
+
+    # custom pkg groups
+    linuxPkgs
+    macosPkgs
+  ];
+}
