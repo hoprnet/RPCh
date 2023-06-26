5b6c3e73282dc196e0ff4b3ff5c9b5c0e427677c
Ronny Esterluss <esterlus@users.noreply.github.com>
2023-06-26 22:34:27 +0200

    enable easy development on nix (#428)
    * initial flake nix setup
    
    * adding insecure to allow nodev16
    
    * updated script header fixes

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "RPCh";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
