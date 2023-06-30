2a42465cc43d50dc9bc361ded6730a9258c26495
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-06-30 14:08:25 +0200

    trying  to fix tests

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,6 +4,7 @@ export { default as Request } from "./request";
 export { default as Response } from "./response";
 export { default as Cache } from "./cache";
 export { default as WebSocketHelper } from "./ws-helper";
+export type { onEventType } from "./ws-helper";
 export * as blockchain from "./blockchain";
 export * as hoprd from "./hoprd";
 export * as utils from "./utils";
