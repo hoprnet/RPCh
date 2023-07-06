73bee25ff7f20f8cbc965ea9f6a2b0d6334dd33b
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-06 08:32:26 +0200

    better handling of error returns

--- packages/sdk/src/nodes-selector.ts
+++ packages/sdk/src/nodes-selector.ts
@@ -1,5 +1,5 @@
-import * as Reliability from './reliability';
-import type { EntryNode, ExitNode } from './nodes-collector';
+import * as Reliability from "./reliability";
+import type { EntryNode, ExitNode } from "./nodes-collector";
 
 /**
  * This algorithm will be subject to change.
@@ -10,38 +10,60 @@ import type { EntryNode, ExitNode } from './nodes-collector';
  *
  * For every entry node there is an LIFO queue of its online state.
  * Every entry node also keeps LIFO queues of their failed/success pairings with exit nodes.
- * For now we just take all exitNodes, combine them with all entry nodes, remove all recent failures.
+ * For now we just take all exit nodes, combine them with all entry nodes, remove all recent failures and ongoing requests.
+ *
+ * This essentially means that each entry - exit node pair can only handle one request concurrently.
+ * This will no scale well, but might be enough to understand reliability problems for now.
  *
  */
-export default function({entryNodes, exitNodes, reliabilities} : { entryNodes: Map<string, EntryNode>, reliabilities: Map<string, Reliability.Reliability>, exitNodes: Map<string, ExitNode>} ): {entryNode: EntryNode, exitNode: ExitNode} | string {
-    const entryIds = Array.from(entryNodes.keys());
-    // 1. determine online entry nodes
-    const onlineIds =entryIds.filter(function(id) {
-        const rel = reliabilities.get(id)!;
-        return Reliability.isOnline(rel);
-    });
+export default function ({
+  entryNodes,
+  exitNodes,
+  reliabilities,
+}: {
+  entryNodes: Map<string, EntryNode>;
+  exitNodes: Map<string, ExitNode>;
+  reliabilities: Map<string, Reliability.Reliability>;
+}):
+  | { res: "ok"; entryNode: EntryNode; exitNode: ExitNode }
+  | { res: "error"; reason: string } {
+  const entryIds = Array.from(entryNodes.keys());
+  if (entryIds.length === 0) {
+    return { res: "error", reason: "no entry nodes" };
+  }
 
-    // 2. gather all tryable entry - exit node pairs
-    const entryExitSelection = onlineIds.map(function(entryId) {
-        const rel = reliabilities.get(entryId)!;
-        // take all exit nodes and remove recent failures
-        const tryableIds = Array.from(exitNodes.keys()).filter(function(exitId) {
-            return !Reliability.isCurrentlyFailure(rel, exitId);
-        })
-        return tryableIds.map(function(exitId) {
-            return { entryNode: entryNodes.get(entryId)!, exitNode: exitNodes.get(exitId)!};
-        })
-    }).flat()
+  // 1. determine online entry nodes
+  const onlineIds = entryIds.filter(function (id) {
+    const rel = reliabilities.get(id)!;
+    return Reliability.isOnline(rel);
+  });
+  if (onlineIds.length === 0) {
+    return { res: "error", reason: "no online entry nodes" };
+  }
 
-    if (entryExitSelection.length === 0) {
-        return "
+  // 2. gather all tryable entry - exit node pairs
+  const entryExitSelection = onlineIds
+    .map(function (entryId) {
+      const rel = reliabilities.get(entryId)!;
+      // take all exit nodes and remove recent failures, coincidentally will remove ongoing requests as well
+      const tryableIds = Array.from(exitNodes.keys()).filter(function (exitId) {
+        return !Reliability.isCurrentlyFailure(rel, exitId);
+      });
+      return tryableIds.map(function (exitId) {
+        return {
+          entryNode: entryNodes.get(entryId)!,
+          exitNode: exitNodes.get(exitId)!,
+        };
+      });
+    })
+    .flat();
 
-    // 6. give a random online node a chance of 1% to avoid total node starvation
-    if (unusedOnlineIds.length > 0 && Math.random() < 0.01) {
-      const id = randomEl(unusedOnlineIds);
-      const entryNode = this.entryNodes.get(id)!;
-    }
-  };
+  if (entryExitSelection.length === 0) {
+    return { res: "error", reason: "no tryable entry - exit pair found" };
+  }
+
+  const el = randomEl(entryExitSelection);
+  return { ...el, res: "ok" };
 }
 
 function randomEl<T>(arr: T[]): T {
