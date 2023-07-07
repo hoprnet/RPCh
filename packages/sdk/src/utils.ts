963254f44b88df0f1586873390a8ad3a1493aab9
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-07 17:36:50 +0200

    readded logger creation

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -1,3 +1,7 @@
+import { utils } from "@rpch/common";
+
+export const createLogger = utils.LoggerFactory("sdk");
+
 export function shortPeerId(peerId: string): string {
   return `p..${peerId.substring(peerId.length - 4)}`;
 }
