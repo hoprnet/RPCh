1fbff3a8c633621ff56be822043c0c3f412dc43a
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-09 12:54:26 +0200

    update reliability to handle request ids

--- packages/sdk/src/reliability.ts
+++ packages/sdk/src/reliability.ts
@@ -3,42 +3,46 @@ import { shortPeerId } from "./utils";
 const FRESH_SCORE = 0.8; // general score when considered fresh
 const FRESHNESS_THRESHOLD = 5; // how many request successes/failures are needed to calculate score
 
+const MAX_ONLINE_HISTORY_LENGTH = 100;
+
 export type OnlineHistoryEntry = { date: number; online: boolean };
 
 export type NodeHistoryEntry = {
   started: number;
   ended?: number;
   success: boolean;
+  peerId: string;
+  requestId: number;
 };
 
 export type Reliability = {
   onlineHistory: OnlineHistoryEntry[]; // LIFO queue
-  exitNodesHistory: Map<string, NodeHistoryEntry[]>; // LIFO queue
+  exitNodesHistory: Map<string, NodeHistoryEntry[]>; // exitNodePeerId -> LIFO queue
+  requestHistory: Map<number, NodeHistoryEntry>; // requestId -> NodeHistoryEntry
 };
 
 export function empty(): Reliability {
   return {
     onlineHistory: [],
     exitNodesHistory: new Map(),
+    requestHistory: new Map(),
   };
 }
 
 export function updateOnline(rel: Reliability, online: boolean): Reliability {
   const entry = { date: Date.now(), online };
   rel.onlineHistory.unshift(entry);
+  while (rel.onlineHistory.length > MAX_ONLINE_HISTORY_LENGTH) {
+    rel.onlineHistory.pop();
+  }
   return rel;
 }
 
 export function expireOlderThan(
-  { onlineHistory, exitNodesHistory }: Reliability,
+  { onlineHistory, exitNodesHistory, requestHistory }: Reliability,
   timeout: number
 ): Reliability {
-  // remove old online history entries
   const threshold = Date.now() - timeout;
-  const onlineIdx = onlineHistory.findIndex(({ date }) => date < threshold);
-  if (onlineIdx >= 0) {
-    onlineHistory = onlineHistory.slice(0, onlineIdx);
-  }
 
   // remove old exit nodes history entries
   for (const id of exitNodesHistory.keys()) {
@@ -54,61 +58,126 @@ export function expireOlderThan(
     }
   }
 
+  // remove old request history entries
+  for (const id of requestHistory.keys()) {
+    const entry = requestHistory.get(id)!;
+    if (entry.ended && entry.ended < threshold) {
+      requestHistory.delete(id);
+    }
+  }
+
   return {
     onlineHistory,
     exitNodesHistory,
+    requestHistory,
   };
 }
 
-export function setExitNodeOngoing(
+export function startRequest(
   rel: Reliability,
-  peerId: string
-): Reliability {
-  const entry = { started: Date.now(), success: false, ongoing: true };
+  peerId: string,
+  requestId: number
+): { res: "ok"; rel: Reliability } | { res: "error"; reason: string } {
+  const logRef = { peerId, requestId };
+
   let hist = rel.exitNodesHistory.get(peerId);
+  if (hist) {
+    const histEntryIdx = hist.findIndex(
+      ({ requestId: rId }) => rId === requestId
+    );
+    if (histEntryIdx >= 0) {
+      return {
+        res: "error",
+        reason: `node history already contains ${logRef}`,
+      };
+    }
+  }
+  if (rel.requestHistory.has(requestId)) {
+    return {
+      res: "error",
+      reason: `request history already contains ${logRef}`,
+    };
+  }
+
+  const entry = {
+    started: Date.now(),
+    success: false,
+    peerId,
+    requestId,
+    ended: undefined,
+  };
   if (hist) {
     hist.unshift(entry);
   } else {
     hist = [entry];
   }
   rel.exitNodesHistory.set(peerId, hist);
-  return rel;
+  rel.requestHistory.set(requestId, entry);
+  return { res: "ok", rel };
 }
 
-export function updateExitNodeSuccess(
+export function finishRequestWithResult(
   rel: Reliability,
   peerId: string,
-  success: boolean
+  requestId: number,
+  result: boolean
 ): { res: "ok"; rel: Reliability } | { res: "error"; reason: string } {
+  const logRef = { peerId, requestId };
   const hist = rel.exitNodesHistory.get(peerId);
   if (!hist) {
     return {
       res: "error",
-      reason: `no history found for ${peerId}`,
+      reason: `no node history found for ${logRef}`,
     };
   }
   if (hist.length === 0) {
     return {
       res: "error",
-      reason: `empty history for ${peerId}`,
+      reason: `empty node history for ${logRef}`,
+    };
+  }
+  const histEntryIdx = hist.findIndex(
+    ({ requestId: rId }) => rId === requestId
+  );
+  if (histEntryIdx < 0) {
+    return {
+      res: "error",
+      reason: `node history does not contain ${logRef}`,
+    };
+  }
+  const reqEntry = rel.requestHistory.get(requestId);
+  if (!reqEntry) {
+    return {
+      res: "error",
+      reason: `no request history entry found for ${logRef}`,
+    };
+  }
+  if (reqEntry.ended) {
+    return {
+      res: "error",
+      reason: `request already finished for ${logRef}`,
     };
   }
 
   const entry = {
-    started: hist[0].started,
+    ...reqEntry,
     ended: Date.now(),
-    success: success,
-    ongoing: false,
+    success: result,
   };
-  hist[0] = entry;
+  hist[histEntryIdx] = entry;
   rel.exitNodesHistory.set(peerId, hist);
+  rel.requestHistory.set(requestId, entry);
   return { res: "ok", rel };
 }
 
 export function isEmpty({
   onlineHistory,
   exitNodesHistory,
+  requestHistory,
 }: Reliability): boolean {
+  if (requestHistory.size > 0) {
+    return false;
+  }
   for (const hist of exitNodesHistory.values()) {
     if (hist.length > 0) {
       return false;
