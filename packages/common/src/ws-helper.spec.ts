bb283b2bae5064272ba30ba886ba60b266cd67ce
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-14 17:09:42 +0200

    all tests

--- packages/common/src/ws-helper.spec.ts
+++ packages/common/src/ws-helper.spec.ts
@@ -44,7 +44,48 @@ describe("test ws class", function () {
       }
     };
     connection = new WebSocketHelper(url, onEvent);
-  }, 100e3);
+  });
+
+  it("reconnects after losing connection", (done) => {
+    const reconnectDelay = 10;
+    let connection: WebSocketHelper;
+    let count = 0;
+    let errorHappened = false;
+    const onEvent: onEventType = (evt) => {
+      switch (evt.action) {
+        case "open":
+          // 1. wait for it to connect first time
+          if (count === 0) {
+            count++;
+            // 2. throw error and force it to reconnect
+            connection["socket"]!.emit(
+              "error",
+              new Error("test: force disconnect")
+            );
+          }
+          // 4. wait for successful connection
+          else if (count == 1) {
+            count++;
+            expect(errorHappened).toBeTruthy();
+            expect(connection["reconnectAttempts"]).toEqual(1);
+            connection.close();
+            done();
+          }
+
+          break;
+        case "error":
+          // 3. wait for reconnection logic to triger
+          errorHappened = true;
+          break;
+        default:
+          break;
+      }
+    };
+    connection = new WebSocketHelper(url, onEvent, {
+      reconnectDelay,
+      maxReconnectAttempts: 3,
+    });
+  });
 
   it("emits an error when ping is not received", (done) => {
     let connection: WebSocketHelper;
