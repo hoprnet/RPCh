b1e1c3e4424d42d725551c17c897a93d5e3a03bf
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-14 15:12:34 +0200

    testing one by one now

--- packages/common/src/ws-helper.spec.ts
+++ packages/common/src/ws-helper.spec.ts
@@ -8,137 +8,35 @@ describe("test ws class", function () {
   let server: WS.Server;
   let httpServer: http.Server;
 
-  beforeEach(function () {
+  it("gets a successful connection", (done) => {
     httpServer = http.createServer();
     server = new WS.Server({ server: httpServer });
     httpServer.listen(1234);
-  });
 
-  afterEach(() => {
-    server.close();
-    httpServer.close();
-  });
-
-  let callcount = 0;
-
-  it("gets a successful connection", (done) => {
-    console.log("FOO_ testing timers");
-    const t = setTimeout(() => {
-      console.log("FOO_ should not happen after");
-    });
-    console.log("FOO_ happens before");
-    clearTimeout(t);
-
-    setTimeout(() => {
-      console.log("FOO_ callcount", callcount++);
-      let connection: WebSocketHelper;
-      let openHappened = false;
-      const onEvent: onEventType = (evt) => {
-        console.log("event", evt.action, evt);
-        switch (evt.action) {
-          // 1. open connection
-          case "open":
-            openHappened = true;
-            connection.close();
-            break;
-          case "message":
-            done("unexpected message");
-            break;
-          case "error":
-            done(`unexpected error: ${evt.event}`);
-            break;
-          // 2. close connection
-          case "close":
-            expect(openHappened).toBe(true);
-            done();
-            break;
-        }
-      };
-      console.log("FOO_ new WebSocketHelper");
-      connection = new WebSocketHelper(url, onEvent);
-      console.log("FOO_ done constructing WebSocketHelper");
-    }, 10);
-  }, 100e3);
-
-  it("reconnects after losing connection", (done) => {
-    const reconnectDelay = 10;
     let connection: WebSocketHelper;
-    let count = 0;
-    let errorHappened = false;
+    let openHappened = false;
     const onEvent: onEventType = (evt) => {
       switch (evt.action) {
+        // 1. open connection
         case "open":
-          // 1. wait for it to connect first time
-          if (count === 0) {
-            count++;
-            // 2. throw error and force it to reconnect
-            connection["socket"]!.emit(
-              "error",
-              new Error("test: force disconnect")
-            );
-          }
-          // 4. wait for successful connection
-          else if (count == 1) {
-            count++;
-            expect(errorHappened).toBe(true);
-            expect(connection["reconnectAttempts"]).toEqual(1);
-            connection.close();
-            done();
-          }
-
-          break;
-        case "error":
-          // 3. wait for reconnection logic to triger
-          errorHappened = true;
-          break;
-        default:
-          break;
-      }
-    };
-    connection = new WebSocketHelper(url, onEvent, {
-      reconnectDelay,
-      maxReconnectAttempts: 3,
-    });
-  });
-
-  it("emits an error when ping is not received", (done) => {
-    let connection: WebSocketHelper;
-    const onEvent: onEventType = (evt) => {
-      switch (evt.action) {
-        case "error":
-          expect(evt.event.message).toContain("heartbeat");
+          openHappened = true;
           connection.close();
+          break;
+        case "message":
+          done("unexpected message");
+          break;
+        case "error":
+          done(`unexpected error: ${evt.event}`);
+          break;
+        // 2. close connection
+        case "close":
+          expect(openHappened).toBe(true);
+          server.close();
+          httpServer.close();
           done();
           break;
       }
     };
-    connection = new WebSocketHelper(url, onEvent, {
-      maxTimeWithoutPing: 100,
-    });
-  });
-
-  it("does not reconnect if instructed to", (done) => {
-    server.close();
-    httpServer.close();
-    let connection: WebSocketHelper;
-    let count = 0;
-    const onEvent: onEventType = (evt) => {
-      switch (evt.action) {
-        case "error": {
-          count++;
-          break;
-        }
-      }
-    };
-
-    connection = new WebSocketHelper(url, onEvent, {
-      maxReconnectAttempts: 0,
-    });
-
-    setTimeout(() => {
-      connection.close();
-      expect(count).toEqual(1);
-      done();
-    }, 1e3);
+    connection = new WebSocketHelper(url, onEvent);
   });
 });
