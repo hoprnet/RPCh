a28c106377f62f82bcd689a470eefde45f280108
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-07-30 21:01:52 +0200

    prepping new structure

--- packages/sdk/src/nodes-collector.ts
+++ packages/sdk/src/nodes-collector.ts
@@ -1,409 +1,176 @@
-import { WebSocketHelper, type onEventType } from "@rpch/common";
-import * as Reliability from "./reliability";
+import { type onEventParameterType } from "@rpch/common";
+
+import * as Nodes from "./nodes";
+import * as NodesAPI from "./nodes-api";
 import { createLogger } from "./utils";
-import selectNodes from "./nodes-selector";
-import { shortPeerId } from "./utils";
 
 const log = createLogger(["nodes-collector"]);
 
-const apiEntryNode = "/api/v1/request/entry-node";
-const apiWebSocket = "/api/v2/messages/websocket";
-const apiExitNode = "/api/v1/node?hasExitNode=true";
-
-const timeoutImmediateFetchEntryNode = 333; // 333 ms
-const timeoutOptimizeFetchEntryNode = 5e3; // 5 sec
-const timeoutRegularFetchEntryNode = 60e3; // 1 min
-
-const timeoutRegularFetchExitNodes = 60e3 * 5; // 5 min
-
-const intervalExpireReliabilities = 60e3 * 5; // 5 min
-
-const intervalDebugPrint = 60e3; // 1 min
-
-let slidingNodesWindow: int = 2;
-
-// these parameters are subject to change and we need to find the best possible combinations
-const latencyThreshold = 5e3;
-const requestThreshold = 15;
-
-export type EntryNode = {
-  apiEndpoint: URL;
-  apiToken: string;
-  peerId: string;
-  connection: any;
-  recommendedExitNodes: ExitNode[];
-};
-
-export type ExitNode = {
-  peerId: string;
-  pubKey: string;
-};
-
-type ResponseEntryNode = {
-  hoprd_api_endpoint: string;
-  accessToken: string;
-  id: string;
-};
-
-type ResponseExitNode = {
-  exit_node_pub_key: string;
-  id: string;
-};
-
-
-const defaultOps: NodesCollectorOps = {
-  entryNodesTarget: 5,
-  maxReliabilityAge: 60e3 * 30, // 30(+/-5) min
-};
-
 export default class NodesCollector {
-  private readonly ops: NodesCollectorOps;
-  private readonly entryNodes = new Map<string, EntryNode>();
-  // will be removed once exit nodes are recommended for entry nodes
-  private readonly genericExitNodes = new Map<string, ExitNode>();
-  private readonly reliabilities = new Map<string, Reliability.Reliability>();
-  private timerRefEntryNodes: ReturnType<typeof setTimeout>;
-  private timerRefExitNodes: ReturnType<typeof setTimeout>;
-  private intervalRefExpiration: ReturnType<typeof setInterval>;
-  private intervalRefDebugPrint: ReturnType<typeof setInterval>;
+  private readonly nodes: Nodes.Nodes = Nodes.init();
+  private actTimer: ReturnType<typeof setTimeout> = setTimeout(function () {});
 
   constructor(
     private readonly discoveryPlatformEndpoint: string,
-    private readonly apiClient: string,
-    private readonly onWSmessage: (peerId: string, message: string) => void,
-    ops: NodesCollectorOps = defaultOps
-  ) {
-    this.ops = {
-      ...defaultOps,
-      ...ops,
-    };
+    private readonly clientId: string,
+    private readonly onWSmessage: (message: string) => void
+  ) {}
 
-    // start fetching entry / exit nodes
-    this.timerRefEntryNodes = setTimeout(() => this.fetchEntryNodes());
-    this.timerRefExitNodes = setTimeout(() => this.fetchExitNodes());
-    this.intervalRefExpiration = setInterval(
-      () => this.expireReliabilities(),
-      intervalExpireReliabilities
-    );
-    this.intervalRefDebugPrint = setInterval(
-      () => this.prettryPrintState(),
-      intervalDebugPrint
-    );
-  }
-
-  public stop = () => {
-    clearTimeout(this.timerRefEntryNodes);
-    clearTimeout(this.timerRefExitNodes);
-    clearInterval(this.intervalRefExpiration);
-    clearInterval(this.intervalRefDebugPrint);
-    this.entryNodes.forEach((node) => node.connection.close());
-    this.entryNodes.clear();
-    this.genericExitNodes.clear();
-    this.reliabilities.clear();
-  };
+  public stop = () => {};
 
   /**
-   * This is the main entry for finding reliable node pair based on recent node behaviours.
+   * Ready for request receival, no websocket yet.
    */
-  public findReliableNodePair = async (
-    timeout: number
-  ): Promise<{ entryNode: EntryNode; exitNode: ExitNode }> => {
+  public ready = async (timeout: number): Promise<boolean> => {
     return new Promise((resolve, reject) => {
       const start = Date.now();
       const check = () => {
         const now = Date.now();
         const elapsed = now - start;
-        const res = selectNodes(
-          this.entryNodes,
-          this.genericExitNodes,
-          this.reliabilities
-        );
-        switch (res.res) {
-          case "ok":
-            resolve({
-              entryNode: res.entryNode,
-              exitNode: res.exitNode,
-            });
-            break;
-          case "error":
-            if (elapsed > timeout) {
-              log.error("Error selecting nodes", res.reason, "after", timeout);
-              reject(`timeout after ${elapsed} ms`);
-            } else {
-              setTimeout(check, 100);
-            }
-            break;
+        const cmd = Nodes.reachReady(this.nodes);
+        if (cmd.label === Nodes.CommandLabel.None) {
+          return resolve(true);
         }
+        this.actOnCmd(cmd);
+        if (elapsed > timeout) {
+          log.error("Timeout waiting for ready", elapsed);
+          return reject(`timeout after ${elapsed} ms`);
+        }
+        setTimeout(check, 100);
       };
       check();
     });
   };
 
-  public startRequest = ({
-    entryId,
-    exitId,
-    requestId,
-  }: {
-    entryId: string;
-    exitId: string;
-    requestId: number;
-  }) => {
-    const logRef = {
-      entryId: shortPeerId(entryId),
-      exitId: shortPeerId(exitId),
-      requestId,
-    };
-    const cur = this.reliabilities.get(entryId);
-    // record only tracked nodes
-    if (!cur) {
-      log.info(`Dropping startRequest for`, logRef);
-      return;
-    }
-
-    const res = Reliability.startRequest(cur, { exitId, requestId });
-    switch (res.res) {
-      case "ok":
-        this.reliabilities.set(entryId, res.rel);
-        break;
-      case "error":
-        log.error(`Error starting request:`, res.reason, "for", logRef);
-        break;
-    }
-  };
-
-  public finishRequest = ({
-    entryId,
-    exitId,
-    requestId,
-    result,
-  }: {
-    entryId: string;
-    exitId: string;
-    requestId: number;
-    result: boolean;
-  }) => {
-    const logRef = {
-      entryId: shortPeerId(entryId),
-      exitId: shortPeerId(exitId),
-      requestId,
-      result,
-    };
-
-    const cur = this.reliabilities.get(entryId);
-    // record only tracked nodes
-    if (!cur) {
-      log.info(`Dropping finishRequest for`, logRef);
-      return;
-    }
-
-    const res = Reliability.finishRequest(cur, { exitId, requestId, result });
-    switch (res.res) {
-      case "ok":
-        this.reliabilities.set(entryId, res.rel);
-        break;
-      case "error":
-        log.error(`Error finishing request:`, res.reason, "for", logRef);
-        break;
-    }
-  };
-
-  private fetchEntryNodes = () => {
-    const excludeList = Array.from(this.entryNodes.keys());
-    const url = new URL(apiEntryNode, this.discoveryPlatformEndpoint);
-    const headers = {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "x-rpch-client": this.apiClient,
-    };
-    const body = JSON.stringify({
-      excludeList,
-      client: this.apiClient,
-    });
-
-    fetch(url, { method: "POST", headers, body })
-      .then((resp) => {
-        if (resp.status === 200) {
-          return resp.json() as unknown as {
-            hoprd_api_endpoint: string;
-            accessToken: string;
-            id: string;
-          };
+  /**
+   * Requested node pair, needs websocket.
+   */
+  public requestNodePair = async (timeout: number): Promise<Nodes.Pair> => {
+    return new Promise((resolve, reject) => {
+      const start = Date.now();
+      const check = () => {
+        const now = Date.now();
+        const elapsed = now - start;
+        const { cmd, nodePair } = Nodes.reachNodePair(this.nodes);
+        if (nodePair) {
+          return resolve(nodePair);
         }
-        throw new Error(`wrong status ${resp.status} ${resp.statusText}`);
-      })
-      .then(this.responseEntryNode)
-      .catch((err) => log.error("Error requesting entry node:", err))
-      .finally(this.scheduleEntryNodeFetching);
+        this.actOnCmd(cmd);
+        if (elapsed > timeout) {
+          log.error("Timeout waiting for node pair", elapsed);
+          return reject(`timeout after ${elapsed} ms`);
+        }
+        setTimeout(check, 100);
+      };
+      check();
+    });
+  };
+
+  public requestStarted = (pair: Nodes.Pair, requestId: number) => {
+    const res = Nodes.requestStarted(this.nodes, pair, requestId);
+    this.actOnCmd(res);
+  };
+
+  public requestSucceeded = (
+    pair: Nodes.Pair,
+    requestId: number,
+    responseTime: number
+  ) => {
+    const res = Nodes.requestSucceeded(
+      this.nodes,
+      pair,
+      requestId,
+      responseTime
+    );
+    this.actOnCmd(res);
+  };
+
+  public requestFailed = (pair: Nodes.Pair, requestId: number) => {
+    const res = Nodes.requestFailed(this.nodes, pair, requestId);
+    this.actOnCmd(res);
+  };
+
+  private actOnCmd = (cmd: Nodes.Command) => {
+    clearTimeout(this.actTimer);
+    switch (cmd.label) {
+      case Nodes.CommandLabel.NeedEntryNode:
+        this.fetchEntryNode(cmd.excludeIds);
+        break;
+      case Nodes.CommandLabel.NeedExitNode:
+        this.fetchExitNodes();
+        break;
+      case Nodes.CommandLabel.OpenWebSocket:
+        this.openWebSocket(cmd.entryNode);
+        break;
+      case Nodes.CommandLabel.CloseWebSocket:
+        this.closeWebSocket(cmd.entryNode);
+        break;
+      default:
+        break;
+    }
+  };
+
+  private fetchEntryNode = (excludeIds: string[]) => {
+    NodesAPI.fetchEntryNode({
+      excludeList: excludeIds,
+      discoveryPlatformEndpoint: this.discoveryPlatformEndpoint,
+      clientId: this.clientId,
+    })
+      .then(this.onEntryNode)
+      .catch(this.onEntryNodeError);
+  };
+
+  private onEntryNode = (entryNode: Nodes.EntryNode) => {
+    Nodes.newEntryNode(this.nodes, entryNode);
+  };
+
+  private onEntryNodeError = (err: string) => {
+    log.error("Error requesting entry node", err);
+    setTimeout(() => {
+      const res = Nodes.reachReady(this.nodes);
+      this.actOnCmd(res);
+    }, 555);
   };
 
   private fetchExitNodes = () => {
-    const url = new URL(apiExitNode, this.discoveryPlatformEndpoint);
-    const headers = {
-      Accept: "application/json",
-      "x-rpch-client": this.apiClient,
-    };
-    fetch(url, { headers })
-      .then((resp) => {
-        if (resp.status === 200) {
-          return resp.json() as unknown as ResponseExitNode[];
+    NodesAPI.fetchExitNodes({
+      discoveryPlatformEndpoint: this.discoveryPlatformEndpoint,
+      clientId: this.clientId,
+    })
+      .then(this.onExitNodes)
+      .catch(this.onExitNodesError);
+  };
+
+  private onExitNodes = (exitNodes: Nodes.ExitNode[]) => {
+    Nodes.addExitNodes(this.nodes, exitNodes);
+  };
+
+  private onExitNodesError = (err: string) => {
+    log.error("Error requesting exit nodes", err);
+    setTimeout(() => {
+      const res = Nodes.reachReady(this.nodes);
+      this.actOnCmd(res);
+    }, 555);
+  };
+
+  private openWebSocket = (entryNode: Nodes.EntryNode) => {
+    const wsConn = NodesAPI.openWebSocket(
+      entryNode,
+      (evt: onEventParameterType) => {
+        switch (evt.action) {
+          case "message":
+            this.onWSmessage(evt.message);
+            break;
+          default:
+            Nodes.onWSevt(this.nodes, entryNode, evt);
+            break;
         }
-        throw new Error(`wrong status ${resp}`);
-      })
-      .then(this.responseExitNode)
-      .catch((err) => log.error("Error requesting exit node:", err))
-      .finally(this.scheduleExitNodeFetching);
-  };
-
-  private responseEntryNode = ({
-    hoprd_api_endpoint,
-    accessToken,
-    id,
-  }: ResponseEntryNode) => {
-    // only use entry node if we not already do so
-    if (this.entryNodes.has(id)) {
-      log.info("Got already tracked entry node", id);
-      return;
-    }
-    const apiEndpoint = new URL(hoprd_api_endpoint);
-    const wsURL = new URL(hoprd_api_endpoint);
-    wsURL.protocol = apiEndpoint.protocol === "https:" ? "wss:" : "ws:";
-    wsURL.pathname = apiWebSocket;
-    wsURL.search = `?apiToken=${accessToken}`;
-    const onEvent = this.onWSevent(id);
-    // create websocket connection
-    const connection = new WebSocketHelper(wsURL, onEvent);
-    const newNode: EntryNode = {
-      apiEndpoint,
-      apiToken: accessToken,
-      connection,
-      peerId: id,
-      recommendedExitNodes: [],
-    };
-    log.info("New entry node", newNode.peerId);
-    this.entryNodes.set(id, newNode);
-    this.reliabilities.set(id, Reliability.empty());
-  };
-
-  private responseExitNode = (resp: ResponseExitNode[]) => {
-    const exitNodes = resp.map(({ exit_node_pub_key, id }) => ({
-      peerId: id,
-      pubKey: exit_node_pub_key,
-    }));
-    log.info(
-      "Response exit nodes",
-      exitNodes.map(({ peerId }) => peerId).join(" ")
-    );
-    exitNodes.forEach((node) => this.genericExitNodes.set(node.peerId, node));
-  };
-
-  private scheduleEntryNodeFetching = () => {
-    clearTimeout(this.timerRefEntryNodes);
-    // immediately fetch new nodes if too few - should be refactored into getting more nodes in one call
-    if (this.entryNodes.size < this.ops.entryNodesTarget) {
-      this.timerRefEntryNodes = setTimeout(
-        () => this.fetchEntryNodes(),
-        timeoutImmediateFetchEntryNode
-      );
-      return;
-    }
-
-    // check online nodes
-    const onlineIds = this.filterOnlineIds();
-    if (onlineIds.length < this.ops.entryNodesTarget) {
-      // fetch new nodes faster if online nodes are missing
-      this.timerRefEntryNodes = setTimeout(
-        () => this.fetchEntryNodes(),
-        timeoutOptimizeFetchEntryNode
-      );
-    } else {
-      // fetch regularly to avoid node starvation
-      this.timerRefEntryNodes = setTimeout(
-        () => this.fetchEntryNodes(),
-        timeoutRegularFetchEntryNode
-      );
-    }
-  };
-
-  private scheduleExitNodeFetching = () => {
-    clearTimeout(this.timerRefExitNodes);
-    // fetch regularly to avoid node starvation
-    this.timerRefExitNodes = setTimeout(
-      () => this.fetchExitNodes(),
-      timeoutRegularFetchExitNodes
-    );
-  };
-
-  private onWSevent = (peerId: string): onEventType => {
-    return (evt) => {
-      switch (evt.action) {
-        case "open":
-        case "ping":
-          this.updateOnlineHistory(peerId, true);
-          break;
-
-        case "message":
-          this.updateOnlineHistory(peerId, true);
-          this.onWSmessage(peerId, evt.message);
-          break;
-
-        case "close":
-        case "error":
-          this.updateOnlineHistory(peerId, false);
-          break;
       }
-    };
+    );
+    Nodes.addWSconn(this.nodes, entryNode, wsConn);
   };
 
-  private updateOnlineHistory = (peerId: string, online: boolean) => {
-    const cur = this.reliabilities.get(peerId);
-    if (cur) {
-      // update only tracked nodes
-      const next = Reliability.updateOnline(cur, online);
-      this.reliabilities.set(peerId, next);
-    }
+  private closeWebSocket = (entryNode: Nodes.EntryNode) => {
+    entryNode.wsConn?.close();
   };
-
-  private filterOnlineIds = () => {
-    return Array.from(this.entryNodes.keys()).filter((pId) => {
-      const rel = this.reliabilities.get(pId);
-      return Reliability.isOnline(rel!);
-    });
-  };
-
-  private expireReliabilities = () => {
-    for (const id of this.reliabilities.keys()) {
-      const rel = this.reliabilities.get(id)!;
-      const next = Reliability.expireOlderThan(rel, this.ops.maxReliabilityAge);
-      if (Reliability.isEmpty(next)) {
-        // remove node
-        this.reliabilities.delete(id);
-        this.entryNodes.delete(id);
-      }
-    }
-  };
-
-  private prettryPrintState() {
-    const exitNodes = Array.from(this.genericExitNodes.values());
-    const exitIds = exitNodes
-      .map(({ peerId }) => shortPeerId(peerId))
-      .join(" ");
-    const exitStr = `${this.genericExitNodes.size} exit nodes: [${exitIds}]`;
-    const entryNodes = Array.from(this.entryNodes.values());
-    const entryIds = entryNodes
-      .map(({ peerId }) => shortPeerId(peerId))
-      .join(" ");
-    const entryStr = `${this.entryNodes.size} entry nodes: [${entryIds}]`;
-    const rels = entryNodes
-      .map(({ peerId }) => {
-        const shortPid = shortPeerId(peerId);
-        const rel = this.reliabilities.get(peerId)!;
-        const onlines = Reliability.prettyPrintOnlineHistory(rel);
-        const exits = Reliability.prettyPrintExitNodesHistory(rel);
-        return [`online ${shortPid}:${onlines}`, `exits ${shortPid}:${exits}`];
-      })
-      .flat();
-    log.verbose(entryStr);
-    log.verbose(exitStr);
-    rels.forEach((l) => log.verbose(l));
-  }
 }
--- packages/sdk/src/nodes.ts
+++ packages/sdk/src/nodes.ts
@@ -1,9 +1,50 @@
+import type { WebSocketHelper, onEventParameterType } from "@rpch/common";
 // these parameters are subject to change and we need to find the best possible combinations
 const latencyThreshold = 5e3;
 const latencyViolationsThreshold = 2;
 const requestThreshold = 5;
 
-export enum WSState {
+export type Nodes = {
+  entryNodes: Map<string, EntryNode>; // peerId -> EntryNode
+  exitNodes: Map<string, ExitNode>; // peerId -> ExitNode
+  outphasing: Set<string>; // no longer eligable entry nodes
+};
+
+export enum CommandLabel {
+  NeedEntryNode,
+  NeedExitNode,
+  OpenWebSocket,
+  CloseWebSocket,
+  None,
+}
+
+export type Command =
+  | { label: CommandLabel.NeedEntryNode; excludeIds: string[] }
+  | { label: CommandLabel.NeedExitNode }
+  | { label: CommandLabel.OpenWebSocket; entryNode: EntryNode }
+  | { label: CommandLabel.CloseWebSocket; entryNode: EntryNode }
+  | { label: CommandLabel.None };
+
+export type Pair = { entryNode: EntryNode; exitNode: ExitNode };
+
+export type EntryNode = {
+  apiEndpoint: URL;
+  accessToken: string;
+  latencyViolations: number;
+  ongoingRequests: number;
+  peerId: string;
+  recommendedExits: Set<string>;
+  wsState: WSstate;
+  wsConn?: WebSocketHelper;
+};
+
+export type ExitNode = {
+  ongoingRequests: number;
+  peerId: string;
+  pubKey: string;
+};
+
+enum WSstate {
   Disconnected,
   Connecting,
   Open,
@@ -11,69 +52,196 @@ export enum WSState {
   Disconnecting,
 }
 
-export type Nodes = {
-  slidingWindow: 2; // current amount of kept nodes
-  entryNodes: Map<string, EntryNode>; // peerId -> EntryNode
-  exitNodes: Map<string, ExitNode>; // peerId -> ExitNode
-  recommendedExits: Map<string, string[]>; // peerId(entryNode) -> peerIds(exitNodes)
-  requestCounts: Map<string, number>; // peerId(entryNode) -> currently ongoing requests
-  lastLatencies: Map<string, number[]>; // peerId(entryNode) -> last latency values (max latencyViolationsThreshold count)
-  webSocketStates: Map<string, WSState>;
-};
-
-export type EntryNode = {
-  apiEndpoint: URL;
-  accessToken: string;
-  peerId: string;
-};
-
-export type ExitNode = {
-  peerId: string;
-  pubKey: string;
-};
-
 export function init(): Nodes {
   return {
-    slidingWindow: 2,
     entryNodes: new Map(),
     exitNodes: new Map(),
-    requestCounts: new Map(),
-    lastLatencies: new Map(),
-    recommendedExits: new Map(),
-    webSocketStates: new Map(),
+    outphasing: new Set(),
+  };
+}
+
+/**
+ * ready state is achieved when we have at least one entry and one exit node.
+ * no websocket connection in this state
+ */
+export function reachReady(nodes: Nodes): Command {
+  // check entry nodes
+  if (nodes.entryNodes.size === 0) {
+    return { label: CommandLabel.NeedEntryNode, excludeIds: [] };
+  }
+
+  // check exit nodes
+  if (nodes.exitNodes.size === 0) {
+    return { label: CommandLabel.NeedExitNode };
+  }
+
+  // check if we have valid entry nodes
+  const entryNodes = Array.from(nodes.entryNodes.values()).filter(
+    ({ peerId }) => !nodes.outphasing.has(peerId)
+  );
+  if (entryNodes.length === 0) {
+    const excludeIds = Array.from(nodes.entryNodes.keys());
+    return { label: CommandLabel.NeedEntryNode, excludeIds: excludeIds };
+  }
+
+  return { label: CommandLabel.None };
+}
+
+/**
+ * node pair can be returned as soon as we have a valid entry / exit node with a connected entry node websocket.
+ */
+export function reachNodePair(nodes: Nodes): { cmd: Command; nodePair?: Pair } {
+  // check entry nodes
+  if (nodes.entryNodes.size === 0) {
+    return { cmd: { label: CommandLabel.NeedEntryNode, excludeIds: [] } };
+  }
+
+  // check exit nodes
+  if (nodes.exitNodes.size === 0) {
+    return { cmd: { label: CommandLabel.NeedExitNode } };
+  }
+
+  // check if we have valid entry nodes
+  const entryNodes = Array.from(nodes.entryNodes.values()).filter(
+    ({ peerId }) => !nodes.outphasing.has(peerId)
+  );
+  if (entryNodes.length === 0) {
+    const excludeIds = Array.from(nodes.entryNodes.keys());
+    return {
+      cmd: { label: CommandLabel.NeedEntryNode, excludeIds: excludeIds },
+    };
+  }
+
+  // check if we have an open websocket entry node
+  const openNodes = entryNodes.filter(({ wsState }) => wsState == WSstate.Open);
+  if (openNodes.length === 0) {
+    // check if we have connecting websocket entry node
+    const connNodes = entryNodes.filter(
+      ({ wsState }) => wsState == WSstate.Connecting
+    );
+    if (connNodes.length === 0) {
+      // no connecting webSocket and no open nodes
+      const entryNode = randomEl(entryNodes);
+      return { cmd: { label: CommandLabel.OpenWebSocket, entryNode } };
+    }
+    // wait for connecting webSocket
+    return { cmd: { label: CommandLabel.None } };
+  }
+
+  // prioritize recommendedExits
+  const entryNode = randomEl(openNodes);
+  const exitNodes = Array.from(nodes.exitNodes.values());
+  const recExits = exitNodes.filter(({ peerId }) =>
+    entryNode.recommendedExits.has(peerId)
+  );
+  if (recExits.length > 0) {
+    const exitNode = randomEl(recExits);
+    return {
+      cmd: { label: CommandLabel.None },
+      nodePair: { entryNode, exitNode },
+    };
+  }
+  const exitNode = randomEl(exitNodes);
+  return {
+    cmd: { label: CommandLabel.None },
+    nodePair: { entryNode, exitNode },
   };
 }
 
 export function newEntryNode(nodes: Nodes, entryNode: EntryNode) {
-  // do nothing if we already track that node
-  if (nodes.entryNodes.has(entryNode.peerId)) {
-    return;
-  }
-  const l = nodes.entryNodes.size;
-  if (l < nodes.slidingWindow) {
-    nodes.entryNodes.set(entryNode.peerId, entryNode);
-    nodes.requestCounts.set(entryNode.peerId, 0);
-    nodes.lastLatencies.set(entryNode.peerId, []);
-    nodes.webSocketStates.set(entryNode.peerId, WSState.Disconnected);
+  nodes.entryNodes.set(entryNode.peerId, entryNode);
+}
+
+export function addExitNodes(nodes: Nodes, exitNodes: ExitNode[]) {
+  exitNodes.forEach((exitNode) =>
+    nodes.exitNodes.set(exitNode.peerId, exitNode)
+  );
+}
+
+export function onWSevt(
+  nodes: Nodes,
+  entryNode: EntryNode,
+  evt: onEventParameterType
+) {
+  switch (evt.action) {
+    case "open":
+      entryNode.wsState = WSstate.Open;
+      break;
+    case "close":
+      entryNode.wsState = WSstate.Disconnected;
+      break;
+    case "error":
+      entryNode.wsState = WSstate.Error;
+      break;
   }
 }
 
-export function needsWebSocket(nodes: Nodes): EntryNode | null {
-  // aim for one open webSocket connection
-  const open = Array.from(nodes.webSocketStates.entries()).filter(
-    ([_, state]) => state === WSState.Open || state === WSState.Connecting
-  );
-  if (open.length > 0) {
-    return null;
+export function addWSconn(
+  nodes: Nodes,
+  entryNode: EntryNode,
+  wsConn: WebSocketHelper
+) {
+  entryNode.wsConn = wsConn;
+}
+
+export function requestStarted(
+  nodes: Nodes,
+  { entryNode, exitNode }: Pair,
+  _requestId: number
+): Command {
+  entryNode.ongoingRequests++;
+  exitNode.ongoingRequests++;
+  if (entryNode.ongoingRequests > requestThreshold) {
+    nodes.outphasing.add(entryNode.peerId);
   }
-  const eligiable = Array.from(nodes.webSocketStates.entries()).filter(
-    ([_, state]) => state === WSState.Disconnected
-  );
-  const el = randomEl(eligiable);
-  if (el) {
-    return nodes.entryNodes.get(el[0])!;
+  return reachReady(nodes);
+}
+
+export function requestSucceeded(
+  nodes: Nodes,
+  pair: Pair,
+  _requestId: number,
+  responseTime: number
+): Command {
+  if (responseTime > latencyThreshold) {
+    pair.entryNode.latencyViolations++;
+    if (pair.entryNode.latencyViolations > latencyViolationsThreshold) {
+      nodes.outphasing.add(pair.entryNode.peerId);
+    }
   }
-  return null;
+  postRequest(nodes, pair);
+  return reachReady(nodes);
+}
+
+export function requestFailed(
+  nodes: Nodes,
+  pair: Pair,
+  _requestId: number
+): Command {
+  nodes.outphasing.add(pair.entryNode.peerId);
+  postRequest(nodes, pair);
+  return reachReady(nodes);
+}
+
+function postRequest(nodes: Nodes, { entryNode, exitNode }: Pair) {
+  entryNode.ongoingRequests--;
+  exitNode.ongoingRequests--;
+
+  // close websocket if possible
+  if (entryNode.ongoingRequests === 0) {
+    entryNode.wsState = WSstate.Disconnecting;
+    entryNode.wsConn.close();
+
+    // check if outphasing entry node
+    if (nodes.outphasing.has(entryNode.peerId)) {
+      nodes.outphasing.delete(entryNode.peerId);
+      nodes.entryNodes.delete(entryNode.peerId);
+      // see if we can opportunistically remove exit node
+      if (exitNode.ongoingRequests === 0) {
+        nodes.exitNodes.delete(exitNode.peerId);
+      }
+    }
+  }
 }
 
 function randomEl<T>(arr: T[]): T {
