f72db3d80cfde295ca9d02760dfe5cf6d8c7cba4
nioni <stivennoni77@gmail.com>
2023-08-03 18:15:07 +0300

    cosmetic SQL change

--- apps/discovery-platform/migrations/1691073458902_quota-sums.sql
+++ apps/discovery-platform/migrations/1691073458902_quota-sums.sql
@@ -4,33 +4,27 @@
 ALTER TABLE public.clients ADD COLUMN quota_paid numeric(78,0) DEFAULT 0 NOT NULL;
 ALTER TABLE public.clients ADD COLUMN quota_used numeric(78,0) DEFAULT 0 NOT NULL;
 
--- UPDATE quota_paid for all existing clients
-UPDATE
-    clients c
-SET
-    quota_paid = r.sum
-FROM
-    (
-        SELECT client_id, SUM(quota) as sum
-        FROM quotas
-        WHERE quota > 0
-        GROUP BY client_id
-    ) r
+-- UPDATE 'quota_paid' for all existing clients
+UPDATE public.clients AS c
+SET quota_paid = r.sum
+FROM (
+    SELECT client_id, SUM(quota) AS sum
+    FROM quotas
+    WHERE quota > 0
+    GROUP BY client_id
+) AS r
 WHERE c.id = r.client_id;
 
--- UPDATE quota_used for all existing clients
-UPDATE
-    clients c
-SET
-    -- we use * -1 since all spending of quota are marked as negatives
-    quota_used = (r.sum * -1)
-FROM
-    (
-        SELECT client_id, SUM(quota) as sum
-        FROM quotas
-        WHERE quota < 0
-        GROUP BY client_id
-    ) r
+-- UPDATE 'quota_used' for all existing clients
+-- we use '* -1' since all spending of quota are marked as negatives
+UPDATE public.clients AS c
+SET quota_used = (r.sum * -1)
+FROM (
+    SELECT client_id, SUM(quota) AS sum
+    FROM quotas
+    WHERE quota < 0
+    GROUP BY client_id
+) AS r
 WHERE c.id = r.client_id;
 
 --------------------------------------------------------------------------------
