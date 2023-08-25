71119a7acb8e1e12f3da9da51eb0488511ef65d8
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-08-25 12:36:17 +0200

    moved all migrations to dp and reinitialized db from scratch

---
+++ apps/discovery-platform/migrations/1692955357433_create-registered-nodes.js
@@ -0,0 +1,25 @@
+const { PgLiteral } = require("node-pg-migrate");
+
+/* eslint-disable camelcase */
+
+exports.shorthands = {
+  id: { type: "uuid", primaryKey: true },
+  createdAt: {
+    type: "timestamp",
+    notNull: true,
+    default: new PgLiteral("current_timestamp"),
+  },
+};
+exports.up = (pgm) => {
+  pgm.createTable("registered_nodes", {
+    id: { type: "varchar(255)", primaryKey: true },
+    is_exit_node: { type: "boolean", notNull: true },
+    chain_id: { type: "integer", notNull: true },
+    hoprd_api_endpoint: { type: "varchar(255)", notNull: true },
+    hoprd_api_token: { type: "varchar(255)", notNull: true },
+    exit_node_pub_key: "varchar(255)",
+    native_address: { type: "varchar(255)", notNull: true },
+    created_at: "createdAt",
+    updated_at: { type: "timestamp", notNull: true },
+  });
+};
