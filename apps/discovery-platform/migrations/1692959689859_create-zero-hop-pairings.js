b0187b8ceb12d2596b0f2205c683ef7f7f799816
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-08-25 12:38:05 +0200

    better migrations

--- apps/discovery-platform/migrations/1692959689859_create-zero-hop-pairings.js
+++ apps/discovery-platform/migrations/1692959689859_create-zero-hop-pairings.js
@@ -1,14 +1,4 @@
-const { PgLiteral } = require("node-pg-migrate");
-
 /* eslint-disable camelcase */
-
-exports.shorthands = {
-  createdAt: {
-    type: "timestamp",
-    notNull: true,
-    default: new PgLiteral("current_timestamp"),
-  },
-};
 
 exports.up = (pgm) => {
   pgm.createTable("zero_hop_pairings", {
