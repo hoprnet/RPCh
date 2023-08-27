6b8bbf084ed2db602131bfeebe266cbecfc3d724
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-08-27 19:27:14 +0200

    better logging

--- apps/availability-monitor/src/availability.ts
+++ apps/availability-monitor/src/availability.ts
@@ -23,90 +23,101 @@ export async function start(dbPool: Pool) {
 async function run(dbPool: Pool) {
   const pEntryNodes = q.entryNodes(dbPool);
   const pExitNodes = q.exitNodes(dbPool);
-  const entryNodes = (await pEntryNodes).rows;
-  const exitNodes = (await pExitNodes).rows;
-  runZeroHops(dbPool, entryNodes, exitNodes).then(() => {
-    // schedule new run every max 10 min
-    const next = Math.floor(Math.random() * 10 * 60e3);
-    const logN = Math.round(next / 1000);
-    log.verbose("scheduling next run in", logN, "s");
-    setTimeout(() => run(dbPool), next);
-  });
-  //runOneHops(entryNodes, exitNodes);
+  Promise.all([pEntryNodes, pExitNodes])
+    .then(([qEntries, qExits]) => {
+      runZeroHops(dbPool, qEntries.rows, qExits.rows);
+      //runOneHops(entryNodes, exitNodes);
+    })
+    .catch((ex) => {
+      log.error("Error querying database", ex);
+      reschedule(dbPool);
+    });
+}
+
+function reschedule(dbPool: Pool) {
+  // schedule new run every max 10 min
+  const next = Math.floor(Math.random() * 10 * 60e3);
+  const logN = Math.round(next / 1000);
+  log.verbose("scheduling next run in", logN, "s");
+  setTimeout(() => run(dbPool), next);
 }
 
 function runZeroHops(
   dbPool: Pool,
-  entryNodes: RegisteredNode[],
-  exitNodes: RegisteredNode[]
-): Promise<void> {
-  return new Promise((res) => {
-    const peersCache: PeersCache.PeersCache = new Map();
+  entryNodes: q.RegisteredNode[],
+  exitNodes: q.RegisteredNode[]
+) {
+  const peersCache: PeersCache.PeersCache = new Map();
 
-    // run everything non blocking
-    const pPairs = entryNodes.map((entry) => {
-      return PeersCache.fetchPeers(peersCache, entry).then((entryPeers) => {
-        const viableExits = exitNodes.filter((x) => entryPeers.has(x.id));
-        return viableExits.map((exit) =>
-          PeersCache.fetchPeers(peersCache, exit).then((exitPeers) => {
-            // check entry node is in quality peers of exit node
-            if (exitPeers.has(entry.id)) {
-              return { entry, exit };
-            }
-            return null;
-          })
-        );
-      });
-    });
-
-    // collect results
-    const allSettled = pPairs.map((p) =>
-      p.then((pExits) => Promise.allSettled(pExits))
-    );
-    Promise.allSettled(allSettled)
-      .then((res) => {
-        const pairings = res.reduce<Pair[]>((outerAcc, outerPrm) => {
-          if ("value" in outerPrm) {
-            const outerPairs = outerPrm.value.reduce<Pair[]>(
-              (innerAcc, innerPrm) => {
-                if ("value" in innerPrm && !!innerPrm.value) {
-                  innerAcc.push(innerPrm.value);
-                }
-                if ("reason" in innerPrm) {
-                  log.info("Encountered rejection", innerPrm.reason);
-                }
-                return innerAcc;
-              },
-              []
-            );
-            return outerPairs;
-          } else {
-            if ("reason" in outerPrm) {
-              log.info("Encountered rejection", outerPrm.reason);
-            }
+  // run everything non blocking
+  const pPairs = entryNodes.map((entry) => {
+    return PeersCache.fetchPeers(peersCache, entry).then((entryPeers) => {
+      const viableExits = exitNodes.filter((x) => entryPeers.has(x.id));
+      return viableExits.map((exit) =>
+        PeersCache.fetchPeers(peersCache, exit).then((exitPeers) => {
+          // check entry node is in quality peers of exit node
+          if (exitPeers.has(entry.id)) {
+            return { entry, exit };
           }
-          return outerAcc;
-        }, []);
-
-        const pairIds = pairings.map(({ entry, exit }) => ({
-          entryId: entry.id,
-          exitId: exit.id,
-        }));
-
-        const logIds = pairIds
-          .map(
-            ({ entryId, exitId }) =>
-              `${shortPeerId(entryId)}>${shortPeerId(exitId)}`
-          )
-          .join(",");
-        // now clear table and insert gathered values
-        q.writeZeroHopPairings(dbPool, pairIds)
-          .then(() => log.verbose("Updated db with pairIds", logIds))
-          .catch((e: any) =>
-            log.error("Error updating db", e, "with pairIds", logIds)
-          );
-      })
-      .catch((err) => log.error("Error during zero hop check", err))
-      .finally(res);
+          return null;
+        })
+      );
+    });
   });
+
+  // collect results
+  const allSettled = pPairs.map((p) =>
+    p.then((pExits) => Promise.allSettled(pExits))
+  );
+  Promise.allSettled(allSettled)
+    .then((res) => {
+      const pairings = res.reduce<Pair[]>((outerAcc, outerPrm) => {
+        if ("value" in outerPrm) {
+          const outerPairs = outerPrm.value.reduce<Pair[]>(
+            (innerAcc, innerPrm) => {
+              if ("value" in innerPrm && !!innerPrm.value) {
+                innerAcc.push(innerPrm.value);
+              }
+              if ("reason" in innerPrm) {
+                log.info(
+                  "Encountered rejection",
+                  JSON.stringify(innerPrm.reason)
+                );
+              }
+              return innerAcc;
+            },
+            []
+          );
+          return outerPairs;
+        } else {
+          if ("reason" in outerPrm) {
+            log.info("Encountered rejection", JSON.stringify(outerPrm.reason));
+          }
+        }
+        return outerAcc;
+      }, []);
+
+      const pairIds = pairings.map(({ entry, exit }) => ({
+        entryId: entry.id,
+        exitId: exit.id,
+      }));
+
+      const logIds = pairIds
+        .map(
+          ({ entryId, exitId }) =>
+            `${shortPeerId(entryId)}>${shortPeerId(exitId)}`
+        )
+        .join(",");
+      // now clear table and insert gathered values
+      q.writeZeroHopPairings(dbPool, pairIds)
+        .then(() => log.verbose("Updated db with pairIds", logIds))
+        .catch((e: any) =>
+          log.error("Error updating db", e, "with pairIds", logIds)
+        )
+        .finally(() => reschedule(dbPool));
+    })
+    .catch((err) => {
+      log.error("Error during zero hop check", err);
+      reschedule(dbPool);
+    });
 }
