ebc0de345ae0a964c1d9bce028ccf4ede52b49b4
Ronny Esterluss <esterlus@users.noreply.github.com>
2023-08-28 13:27:09 +0200

    generate priv public keypair for exit node usage (#485)
    * added keypair generation script
    
    * print as single line
    
    * adjusted to better output
    
    * Change format
    
    * updated output to reduce confusion
    
    * Format to JSON
    
    ---------
    
    Co-authored-by: ausias-armesto <ausiasarmesto@gmail.com>

---
+++ apps/exit-node/gen-keypair.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const { Wallet, utils } = require("ethers");
+
+//console.log("generating random wallet");
+const wallet = Wallet.createRandom();
+const compressedPubkey = utils.computePublicKey(wallet.publicKey, true);
+const output = {
+  private_key: wallet.privateKey,
+  public_key: compressedPubkey,
+};
+console.log(JSON.stringify(output));
