2d5a9ad72879e11ce80e910dc179f75edc5fd4d3
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-08-28 13:18:46 +0200

    adjusted to better output

--- apps/exit-node/gen-keypair.js
+++ apps/exit-node/gen-keypair.js
@@ -4,9 +4,9 @@ const { Wallet, utils } = require("ethers");
 console.log("generating random wallet");
 const wallet = Wallet.createRandom();
 const compressedPubkey = utils.computePublicKey(wallet.publicKey, true);
-console.log(
-  "privkey:",
-  wallet.privateKey,
-  "compressedPubkey",
-  compressedPubkey
-);
+const output = {
+  private_key: wallet.privateKey,
+  public_key: wallet.publicKey,
+  compressed_pubkey: compressedPubkey,
+};
+console.log("generated:", output);
