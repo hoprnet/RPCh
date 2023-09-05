3a99cf7d69bf6553b1a416715f14b7237d73d5c4
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-05 21:10:15 +0200

    better node selection tests

--- packages/sdk/src/node-selector.spec.ts
+++ packages/sdk/src/node-selector.spec.ts
@@ -6,13 +6,37 @@ import type { ExitNode } from "./exit-node";
 
 describe("test node selector", function () {
   it("selects none if no information is given", function () {
-    const entryNode = genEntryNode("e1");
-    const exitNode = genExitNode("x1");
-    entryNode.recommendedExits.push(exitNode.id);
-    const np = NodePair.create(entryNode, [exitNode], 0, () => {});
+    const en = genEntryNode("e1");
+    const xn = genExitNode("x1");
+    en.recommendedExits.push(xn.id);
+    const np = NodePair.create(en, [xn], 0, () => {});
     const nodePairs = new Map([[NodePair.id(np), np]]);
     const res = NodeSel.routePair(nodePairs);
     expect(res).toMatchObject({ success: false, error: "none available" });
+  });
+
+  it("selects quickest ping if no other information is given", function () {
+    const en1 = genEntryNode("e1");
+    const xn1 = genExitNode("x1");
+    en1.recommendedExits.push(xn1.id);
+    const en2 = genEntryNode("e2");
+    const xn2 = genExitNode("x2");
+    en2.recommendedExits.push(xn2.id);
+    const np1 = NodePair.create(en1, [xn1], 0, () => {});
+    const np2 = NodePair.create(en2, [xn2], 0, () => {});
+    np1.pingDuration = 20;
+    np2.pingDuration = 10;
+    const nodePairs = new Map([
+      [NodePair.id(np1), np1],
+      [NodePair.id(np2), np2],
+    ]);
+    const res = NodeSel.routePair(nodePairs);
+    expect(res).toMatchObject({
+      success: true,
+      entryNode: en2,
+      exitNode: xn2,
+      via: "quickest ping",
+    });
   });
 });
 
