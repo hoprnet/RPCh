0bd53229f0fb8dda078e8b895884537f44241ebf
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-08 14:40:53 +0200

    added strategy for passportjs

--- apps/discovery-platform/src/entry-server/routers/v1/login.ts
+++ apps/discovery-platform/src/entry-server/routers/v1/login.ts
@@ -9,34 +9,71 @@ import type { Pool, QueryResult } from "pg";
 
 const log = createLogger(["router", "login"]);
 
+const chainId = "eip155:1";
+
+type VerifyCb = (err?: Error, userId?: string) => {};
+
 export function setup(dbPool: Pool) {
   const store = new SessionNonceStore();
 
   passport.use(
     new EthereumStrategy({ store: store }, function verify(
       address: string,
-      cb: (err: Error) => {}
+      cb: VerifyCb
     ) {
-      q.readLogin(dbPool, address, "eip155:1")
-        .then((res) => login(dbPool, address, res))
+      q.readLogin(dbPool, address, chainId)
+        .then((res) => login(dbPool, address, res, cb))
         .catch((err) => {
           log.error("Error during readLogin query", err);
           cb(err);
         });
     })
   );
-
-  function login(
-    dbPool: Pool,
-    address: string,
-    res: QueryResult<{ user_id: string }>
-  ) {
-    if (res.rowCount === 0) {
-      // create user
-      q.createUser(dbPool, {}).then((res) => res);
-    }
-  }
 }
+
+async function login(
+  dbPool: Pool,
+  address: string,
+  res: QueryResult<{ user_id: string }>,
+  cb: (err?: Error, userId?: string) => {}
+) {
+  if (res.rowCount === 0) {
+    return createLogin(dbPool, address, cb);
+  }
+  if (res.rowCount === 1) {
+    return cb(undefined, res.rows[0].user_id);
+  }
+  const reason = "Wrong rowCount from readLogin query";
+  log.error(reason, res);
+  return cb(new Error(reason));
+}
+
+function createLogin(dbPool: Pool, address: string, cb: VerifyCb) {
+  q.createUser(dbPool, {})
+    .then((res) => {
+      if (res.rowCount !== 1) {
+        const reason = "Wrong rowCount from createUser query";
+        log.error(reason, res);
+        return cb(new Error(reason));
+      }
+      const userId = res.rows[0].id;
+      q.createChainCredential(dbPool, {
+        user_id: userId,
+        address,
+        chain: chainId,
+      })
+        .then(() => cb(undefined, userId))
+        .catch((err) => {
+          log.error("Error during createChainCredential query", err);
+          return cb(err);
+        });
+    })
+    .catch((err) => {
+      log.error("Error during createUser query", err);
+      return cb(err);
+    });
+}
+
 //
 //
 //     ], function(err, row) {
