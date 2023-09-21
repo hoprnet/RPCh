c356d3f180a13d76b9bcf37707b8257e271f9eb5
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-21 09:39:41 +0200

    add federated creds table

---
+++ apps/discovery-platform/migrations/1695279352947_create-federated-credentials.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  // add missing index from previous migration
+  pgm.createIndex("chain_credentials", "user_id");
+
+  pgm.createTable("federated_credentials", {
+    user_id: { type: "uuid", references: "users", notNull: "true" },
+    provider: { type: "varchar(255)", notNull: true },
+    subject: { type: "varchar(255)", notNull: true },
+  });
+
+  pgm.createIndex("federated_credentials", "user_id");
+};
