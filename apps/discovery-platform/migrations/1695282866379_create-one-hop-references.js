2674a14272915fe1c1b170b4ac318e2c94f51e3b
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-21 09:57:43 +0200

    create one hope table - fix zero hop table

---
+++ apps/discovery-platform/migrations/1695282866379_create-one-hop-references.js
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("one_hop_pairings", {
+    entry_id: { type: "uuid", references: "registered_nodes", notNull: "true" },
+    exit_id: { type: "uuid", references: "registered_nodes", notNull: "true" },
+    created_at: "createdAt",
+  });
+
+  pgm.createIndex("one_hop_pairings", ["entry_id", "exit_id"], {
+    unique: true,
+  });
+
+  pgm.createIndex("one_hop_pairings", "entry_id");
+  pgm.createIndex("one_hop_pairings", "exit_id");
+
+  // add missing zero hop indexes
+  pgm.createIndex("zero_hop_pairings", "entry_id");
+  pgm.createIndex("zero_hop_pairings", "exit_id");
+};
