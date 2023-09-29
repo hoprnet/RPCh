ae13caf9f3ba125bbc3bba132c8af8fe418f2f3b
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-29 12:27:24 +0200

    added packages migration

---
+++ apps/discovery-platform/migrations/1695973820206_restructure-quota-into-packages.js
@@ -0,0 +1,78 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.dropTable("packages_quota_bundles");
+  pgm.dropTable("vouchers_quota_bundles");
+  pgm.dropTable("quota_bundles");
+
+  // table is empty so we can add not null column directly
+  pgm.addColumn("packages", {
+    segment_count_per_month: { type: "integer", notNull: true },
+  });
+  // disable this until we have billing_schemes
+  pgm.alterColumn("packages", "billing_scheme_id", {
+    type: "uuid",
+    references: "billing_scheme",
+    notNull: false,
+  });
+
+  // create nullable -> add data -> make not nullable
+  pgm.addColumn("vouchers", {
+    package_id: { type: "uuid", references: "packages" },
+  });
+
+  createDefaultPackages(pgm);
+  createDefaultVouchers(pgm);
+  updateExistingVouchers(pgm);
+};
+
+function createDefaultPackages(pgm) {
+  const sql = [
+    "insert into packages (id, name, desc, segment_count_per_month, created_at) values",
+    "(gen_random_uuid(), {name1}, {desc1}, {segs1}, now()),",
+    "(gen_random_uuid(), {name2}, {desc2}, {segs2}, now()),",
+    "(gen_random_uuid(), {name3}, {desc3}, {segs3}, now())",
+  ].join(" ");
+  const vals = {
+    name1: "casual",
+    desc1: "3 trades per week, 3 hours of DeFi browsing",
+    segs1: 178e3,
+    name2: "trader",
+    desc2: "12 trades per week, 12 hours of DeFi browsing",
+    segs2: 605.2e3,
+    name3: "degen",
+    desc3: "28 trades per week, 28 hours of DeFi browsing",
+    segs3: 1424e3,
+  };
+  pgm.sql(sql, vals);
+}
+
+function createDefaultVouchers(pgm) {
+  createVoucher(pgm, {
+    packageName: "degen",
+    name: "default sign up voucher",
+    code: "FREEPRIVACY",
+    validUntil: new Date("2025-01-01"),
+  });
+  createVoucher(pgm, {
+    packageName: "degen",
+    name: "eth rome voucher",
+    code: "ETHROMEHACKERS",
+    validUntil: new Date("2023-11-01"),
+  });
+}
+
+function createVoucher(pgm, vals) {
+  const sql = [
+    "insert into vouchers (id, package_id, name, code, valid_until, created_at)",
+    "values (gen_random_uuid(), (select id from packages where name = {packageName}), {name}, {code}, {validUntil}, now())",
+    "where not exists (select id from vouchers where code = {code})",
+  ].join(" ");
+  pgm.sql(sql, vals);
+}
+
+function updateExistingVouchers(pgm) {
+  const sql =
+    "update vouchers set package_id = (select id from packages where name = {packageName});";
+  pgm.sql(sql, { packageName: "degen" });
+}
