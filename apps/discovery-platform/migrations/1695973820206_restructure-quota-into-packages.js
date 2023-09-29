6f2a630a3761d1a655fe476b03512457799f164e
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-09-29 12:56:30 +0200

    updated miagrations to actually work

--- apps/discovery-platform/migrations/1695973820206_restructure-quota-into-packages.js
+++ apps/discovery-platform/migrations/1695973820206_restructure-quota-into-packages.js
@@ -24,55 +24,48 @@ exports.up = (pgm) => {
   createDefaultPackages(pgm);
   createDefaultVouchers(pgm);
   updateExistingVouchers(pgm);
+
+  pgm.alterColumn("vouchers", {
+    package_id: { type: "uuid", references: "packages", notNull: true },
+  });
+
+  pgm.createIndex("vouchers", "package_id");
 };
 
 function createDefaultPackages(pgm) {
   const sql = [
-    "insert into packages (id, name, desc, segment_count_per_month, created_at) values",
-    "(gen_random_uuid(), {name1}, {desc1}, {segs1}, now()),",
-    "(gen_random_uuid(), {name2}, {desc2}, {segs2}, now()),",
-    "(gen_random_uuid(), {name3}, {desc3}, {segs3}, now())",
+    'insert into packages (id, name, "desc", segment_count_per_month, created_at) values',
+    "(gen_random_uuid(), 'casual', '3 trades per week - 3 hours of DeFi browsing', 178000, now()),",
+    "(gen_random_uuid(), 'trader', '12 trades per week - 12 hours of DeFi browsing', 605200, now()),",
+    "(gen_random_uuid(), 'degen', '28 trades per week - 28 hours of DeFi browsing', 1424000, now())",
   ].join(" ");
-  const vals = {
-    name1: "casual",
-    desc1: "3 trades per week, 3 hours of DeFi browsing",
-    segs1: 178e3,
-    name2: "trader",
-    desc2: "12 trades per week, 12 hours of DeFi browsing",
-    segs2: 605.2e3,
-    name3: "degen",
-    desc3: "28 trades per week, 28 hours of DeFi browsing",
-    segs3: 1424e3,
-  };
-  pgm.sql(sql, vals);
+  pgm.sql(sql);
 }
 
 function createDefaultVouchers(pgm) {
   createVoucher(pgm, {
-    packageName: "degen",
     name: "default sign up voucher",
     code: "FREEPRIVACY",
-    validUntil: new Date("2025-01-01"),
+    validUntil: "2025-01-01",
   });
   createVoucher(pgm, {
-    packageName: "degen",
     name: "eth rome voucher",
     code: "ETHROMEHACKERS",
-    validUntil: new Date("2023-11-01"),
+    validUntil: "2023-11-01",
   });
 }
 
-function createVoucher(pgm, vals) {
+function createVoucher(pgm, { name, code, validUntil }) {
   const sql = [
     "insert into vouchers (id, package_id, name, code, valid_until, created_at)",
-    "values (gen_random_uuid(), (select id from packages where name = {packageName}), {name}, {code}, {validUntil}, now())",
-    "where not exists (select id from vouchers where code = {code})",
+    `select gen_random_uuid(), (select id from packages where name = 'degen'), '${name}', '${code}', '${validUntil}', now()`,
+    `where not exists (select id from vouchers where code = '${code}')`,
   ].join(" ");
-  pgm.sql(sql, vals);
+  pgm.sql(sql);
 }
 
 function updateExistingVouchers(pgm) {
   const sql =
-    "update vouchers set package_id = (select id from packages where name = {packageName});";
-  pgm.sql(sql, { packageName: "degen" });
+    "update vouchers set package_id = (select id from packages where name = 'degen');";
+  pgm.sql(sql);
 }
