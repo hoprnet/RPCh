d9d6995d7dcce64932f7ebf4921221707e5af5b7
Diego Alzate <diego35.da@gmail.com>
2023-10-03 08:14:28 -0500

    add webhook table migration

---
+++ apps/discovery-platform/migrations/1696338762348_add-webhook-logs.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  // Create the webhook_logs table
+  pgm.createTable("webhook_logs", {
+    id: {
+      type: "uuid",
+      default: pgm.func("gen_random_uuid()"),
+      primaryKey: true,
+      notNull: true,
+      comment: "Identifier of the log entry",
+    },
+    event_type: {
+      type: "varchar(255)",
+      notNull: true,
+      comment: "Type of the event, e.g., Subscription.Created",
+    },
+    event_data: {
+      type: "jsonb",
+      notNull: true,
+      comment: "Payload of the webhook event",
+    },
+    received_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+      comment: "When the webhook was received",
+    },
+  });
+
+  // Optionally, add indexes for commonly queried fields
+  pgm.createIndex("webhook_logs", "event_type");
+  pgm.createIndex("webhook_logs", "received_at");
+};
+
+exports.down = (pgm) => {
+  // Drop the webhook_logs table
+  pgm.dropTable("webhook_logs");
+};
