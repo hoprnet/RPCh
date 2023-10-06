2778996ff5ba4c7e9f25ec16dbb4ec1f2502690a
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-10-06 12:09:34 +0200

    reduce to join table

--- apps/discovery-platform/migrations/1696586458999_create-boomfi-packages.js
+++ apps/discovery-platform/migrations/1696586458999_create-boomfi-packages.js
@@ -2,11 +2,9 @@
 
 exports.up = (pgm) => {
   pgm.createTable("boomfi_packages", {
-    id: "id",
     boomfi_package_id: { type: "varchar(255)", unique: true, notNull: true },
     package_id: { type: "uuid", references: "packages", notNull: true },
     created_at: "createdAt",
-    updated_at: "timestamp",
   });
 
   pgm.createIndex("boomfi_packages", "package_id");
