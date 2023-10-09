ae87b5dd92c66dafb67b1c3fa3efed363941f148
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-10-09 11:27:16 +0200

    initial crypto package

---
+++ packages/crypto/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_ignored' },
+        ],
+    },
+};
