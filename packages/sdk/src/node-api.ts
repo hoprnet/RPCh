0324d692b114b5425010a3f3467ba2b84bae9ddd
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-10-24 14:18:47 +0200

    consolidated node-api

--- packages/sdk/src/node-api.ts
+++ packages/sdk/src/node-api.ts
@@ -8,6 +8,46 @@ export type ConnInfo = { apiEndpoint: URL; accessToken: string };
 
 export type Message = { tag: number; body: string };
 
+export type Heartbeats = {
+  sent: number;
+  success: number;
+};
+
+export type Peer = {
+  peerId: string;
+  multiAddr: string;
+  heartbeats: Heartbeats[];
+  lastSeen: number;
+  quality: number;
+  backoff: number;
+  isNew: boolean;
+  reportedVersion: string;
+};
+
+export type Peers = {
+  connected: Peer[];
+  announced: Peer[];
+};
+
+export type Channel = {
+  channelId: string;
+  sourceAddress: string;
+  destinationAddress: string;
+  sourcePeerId: string;
+  destinationPeerId: string;
+  balance: string;
+  status: string;
+  ticketIndex: string;
+  channelEpoch: string;
+  closureTime: string;
+};
+
+export type Channels = {
+  all: Channel[];
+  incoming: [];
+  outgoing: [];
+};
+
 export function connectWS(conn: ConnInfo): WebSocket {
   const wsURL = new URL("/api/v3/messages/websocket", conn.apiEndpoint);
   wsURL.protocol = conn.apiEndpoint.protocol === "https:" ? "wss:" : "ws:";
@@ -82,4 +122,26 @@ export function accountAddresses(conn: ConnInfo) {
   return fetch(url, { headers }).then((res) => {
     return res.json() as unknown as { native: string; hopr: string };
   });
+}
+
+export function getPeers(conn: ConnInfo): Promise<Peers> {
+  const url = new URL("/api/v3/node/peers", conn.apiEndpoint);
+  url.searchParams.set("quality", "1");
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "x-auth-token": conn.accessToken,
+  };
+  return fetch(url, { headers }).then((res) => res.json());
+}
+
+export function getChannels(conn: ConnInfo): Promise<Channels> {
+  const url = new URL("/api/v3/channels", conn.apiEndpoint);
+  url.searchParams.set("fullTopology", "true");
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "x-auth-token": conn.accessToken,
+  };
+  return fetch(url, { headers }).then((res) => res.json());
 }
