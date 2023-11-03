a14df66ca72a765c239d2edcb7b60ab79af26c77
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-11-03 15:18:49 +0100

    consolidated eslint rc files

--- apps/exit-node/.eslintrc.js
+++ apps/exit-node/.eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
     env: {
-        es2018: true,
+        node: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-    rules: {
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            { argsIgnorePattern: '^_', varsIgnorePattern: '^_ignored' },
-        ],
-        '@typescript-eslint/no-explicit-any': ['off'],
-    },
+    extends: '../../.eslintrc.js',
 };
