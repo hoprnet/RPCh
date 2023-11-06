d7b7167a6dc91bf93a69a83bf617e4d33d8444c9
Diego Alzate <diego35.da@gmail.com>
2023-11-06 12:07:17 -0500

    clean up config

--- examples/ethers/.eslintrc.js
+++ examples/ethers/.eslintrc.js
@@ -1,22 +1,6 @@
 module.exports = {
     env: {
         browser: true,
-        es2018: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:compat/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'compat'],
-    root: true,
-    rules: {
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            { argsIgnorePattern: '^_', varsIgnorePattern: '^_ignored' },
-        ],
-        '@typescript-eslint/no-explicit-any': ['off'],
-    },
+    extends: '../../.eslintrc.js',
 };
