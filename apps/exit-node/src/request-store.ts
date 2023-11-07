72b81b9b55bf84cfae52882d689a34b525c7c3a4
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-11-07 11:18:48 +0100

    initial request-store

---
+++ apps/exit-node/src/request-store.ts
@@ -0,0 +1,49 @@
+import sqlite3 from 'sqlite3';
+// import { Utils } from '@rpch/sdk';
+
+// const log = Utils.logger(['exit-node', 'request-store']);
+
+export type RequestStore = {
+    db: sqlite3.Database;
+};
+
+export function setup(dbFile: string): Promise<boolean> {
+    return new Promise((res, rej) => {
+        const db = new sqlite3.Database(dbFile, (err) => {
+            if (err) {
+                rej(`Error creating db: ${err}`);
+            }
+        });
+
+        db.serialize(() => {
+            db.run(
+                'CREATE TABLE IF NOT EXISTS request_store (uuid TEXT PRIMARY KEY, counter INTEGER)',
+                (err) => {
+                    if (err) {
+                        rej(`Error creating table request_store: ${err}`);
+                    }
+                }
+            );
+
+            db.run(
+                'CREATE INDEX IF NOT EXISTS request_store_counter_index ON request_store (counter)',
+                (err) => {
+                    if (err) {
+                        rej(`Error creating index request_store_counter_index: ${err}`);
+                    }
+                    res(true);
+                }
+            );
+        });
+    });
+}
+
+export function close({ db }: RequestStore): Promise<void> {
+    return new Promise((res, rej) => {
+        db.close((err) => {
+            if (err) {
+                rej(`Error closing db: ${err}`);
+            }
+        });
+    });
+}
