5ba34ae99a0be1b1d0e61dd7b0fa0b166a47e81b
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-11-09 12:17:07 +0100

    updated docker files with more natural build folder

--- apps/discovery-platform/src/index.ts
+++ apps/discovery-platform/src/index.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import migrate from 'node-pg-migrate';
 import { Pool } from 'pg';
 import { Utils } from '@rpch/sdk';
@@ -17,11 +16,7 @@ const start = async (ops: {
     secrets: Secrets;
     url: string;
 }) => {
-    // run db migrations - different dirs inside and outside container
-    let migrationsDirectory = path.join(__dirname, './migrations');
-    if (!fs.existsSync(migrationsDirectory)) {
-        migrationsDirectory = path.join(__dirname, '../migrations');
-    }
+    const migrationsDirectory = path.join(__dirname, '../migrations');
 
     await migrate({
         direction: 'up',
