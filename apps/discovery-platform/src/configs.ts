cd9a7210fcd5bda150bb6222c6fc6bb224ca03c8
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2023-11-21 06:15:06 +0100

    return config versions with initial pairings query

---
+++ apps/discovery-platform/src/configs.ts
@@ -0,0 +1,12 @@
+import * as pg from 'pg';
+
+export enum Keys {
+    RPCh_DOCKER_IMAGE_VERSION = 'RPCh_DOCKER_IMAGE_VERSION',
+    RPCh_SDK_VERSION = 'RPCh_SDK_VERSION',
+}
+
+export async function readConfig(dbPool: pg.Pool, key: Keys): Promise<string | undefined> {
+    const q = 'select data from configs where key = $1';
+    const { rows } = await dbPool.query(q, [key]);
+    return rows[0]?.data;
+}
