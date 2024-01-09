6bc849c47d45fbfaa7c8a6a60e1b3faaf276e7c2
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-01-09 14:14:59 +0100

    fixed formatting job for examples and e2e

--- devkit/e2e/src/ethers-integration.spec.ts
+++ devkit/e2e/src/ethers-integration.spec.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import RPChSDK, { type Ops } from '@rpch/sdk';
-import { RPChProvider } from '../../../examples/ethers/build/index'
+import { RPChProvider } from '../../../examples/ethers/build/index';
 
 const SECRET = 'foobarfoobar';
 const PROVIDER_URL = 'https://gnosis-provider.rpch.tech';
@@ -9,35 +9,34 @@ const DISCOVERY_PLATFORM_API_ENDPOINT = 'http://localhost:3020';
 jest.setTimeout(1e3 * 60 * 1); // one minute
 
 const ops: Ops = {
-  discoveryPlatformEndpoint: DISCOVERY_PLATFORM_API_ENDPOINT,
-  provider: PROVIDER_URL,
-  forceZeroHop: true
-}
+    discoveryPlatformEndpoint: DISCOVERY_PLATFORM_API_ENDPOINT,
+    provider: PROVIDER_URL,
+    forceZeroHop: true,
+};
 
 describe('ethers-integration tests', function () {
-  let sdk:any, provider:any;
+    let sdk: any, provider: any;
 
-  beforeAll(() => {
-    sdk = new RPChSDK(SECRET, ops);
-    provider = new RPChProvider(DISCOVERY_PLATFORM_API_ENDPOINT, sdk);
-    return sdk.isReady();
-  }, 30_000);
+    beforeAll(() => {
+        sdk = new RPChSDK(SECRET, ops);
+        provider = new RPChProvider(DISCOVERY_PLATFORM_API_ENDPOINT, sdk);
+        return sdk.isReady();
+    }, 30_000);
 
-  afterAll(() => {
-    sdk.destruct();
-    // wait for close events to happen
-    return new Promise((resolve) => setTimeout(resolve, 1_000));
-  });
+    afterAll(() => {
+        sdk.destruct();
+        // wait for close events to happen
+        return new Promise((resolve) => setTimeout(resolve, 1_000));
+    });
 
+    it('should get block number from default online PRCh provider', async function () {
+        const blockNumberString = await provider.send('eth_blockNumber', []);
+        const blockNumber = BigInt(blockNumberString);
+        assert.equal(typeof blockNumber, 'bigint');
+    });
 
-  it('should get block number from default online PRCh provider', async function () {
-    const blockNumberString = await provider.send('eth_blockNumber', []);
-    const blockNumber = BigInt(blockNumberString);
-    assert.equal(typeof blockNumber, 'bigint');
-  });
-
-  it('should get chain id from default online PRCh provider', async function () {
-    const chainId = await provider.send('eth_chainId', []);
-    assert.equal(chainId, '0x64');
-  });
+    it('should get chain id from default online PRCh provider', async function () {
+        const chainId = await provider.send('eth_chainId', []);
+        assert.equal(chainId, '0x64');
+    });
 });
--- devkit/e2e/src/rpch-sdk-zero-hop.spec.ts
+++ devkit/e2e/src/rpch-sdk-zero-hop.spec.ts
@@ -7,58 +7,57 @@ const DISCOVERY_PLATFORM_API_ENDPOINT = 'http://localhost:3020';
 
 jest.setTimeout(1e3 * 60 * 1); // one minute
 
-
 describe('rpch-sdk-zero-hop tests', function () {
-  let sdk:any;
+    let sdk: any;
 
-  beforeAll(() => {
-    sdk = setupSDK();
-    return sdk.isReady();
-  }, 30_000);
+    beforeAll(() => {
+        sdk = setupSDK();
+        return sdk.isReady();
+    }, 30_000);
 
-  afterAll(() => {
-    sdk.destruct();
-    // wait for close events to happen
-    return new Promise((resolve) => setTimeout(resolve, 1_000));
-  });
+    afterAll(() => {
+        sdk.destruct();
+        // wait for close events to happen
+        return new Promise((resolve) => setTimeout(resolve, 1_000));
+    });
 
-  it('should get block number from default online PRCh provider', async function () {
-    const response = await sdk.send(
-      {
-        method: 'eth_blockNumber',
-        params: [],
-        jsonrpc: '2.0',
-      },
-      {
-        provider: PROVIDER_URL,
-      },
-    );
-    const json = await response.json();
-    const blockNumber = BigInt(json.result);
-    assert.equal(typeof blockNumber, 'bigint');
-  });
+    it('should get block number from default online PRCh provider', async function () {
+        const response = await sdk.send(
+            {
+                method: 'eth_blockNumber',
+                params: [],
+                jsonrpc: '2.0',
+            },
+            {
+                provider: PROVIDER_URL,
+            },
+        );
+        const json = await response.json();
+        const blockNumber = BigInt(json.result);
+        assert.equal(typeof blockNumber, 'bigint');
+    });
 
-  it('should get chain id from default online PRCh provider', async function () {
-    const response = await sdk.send(
-      {
-        method: 'eth_chainId',
-        params: [],
-        jsonrpc: '2.0',
-      },
-      {
-        provider: PROVIDER_URL,
-      },
-    );
-    const json = await response.json();
-    const chainId = json.result;
-    assert.equal(chainId, '0x64');
-  });
+    it('should get chain id from default online PRCh provider', async function () {
+        const response = await sdk.send(
+            {
+                method: 'eth_chainId',
+                params: [],
+                jsonrpc: '2.0',
+            },
+            {
+                provider: PROVIDER_URL,
+            },
+        );
+        const json = await response.json();
+        const chainId = json.result;
+        assert.equal(chainId, '0x64');
+    });
 });
 
 function setupSDK() {
-  const ops: Ops = {
-    discoveryPlatformEndpoint: DISCOVERY_PLATFORM_API_ENDPOINT,
-    forceZeroHop: true
-  }
-  return new RPChSDK(SECRET, ops);
+    const ops: Ops = {
+        discoveryPlatformEndpoint: DISCOVERY_PLATFORM_API_ENDPOINT,
+        forceZeroHop: true,
+    };
+    return new RPChSDK(SECRET, ops);
 }
