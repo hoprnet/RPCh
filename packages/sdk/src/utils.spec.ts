efdafeb44f8cc202ebd62cabe0bc830bb4f6d1eb
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-01-24 13:40:32 +0100

    added custom arrayify function

--- packages/sdk/src/utils.spec.ts
+++ packages/sdk/src/utils.spec.ts
@@ -1,5 +1,6 @@
 import * as utils from './utils';
 import * as Res from './result';
+import { utils as ethersUtils } from 'ethers';
 
 describe('test utils', function () {
     it('compares versions', function () {
@@ -20,5 +21,14 @@ describe('test utils', function () {
         expect(res.res).toBe(utils.VrsnCmp.MinorMismatch);
         res = utils.versionCompare('', '1.4.0');
         expect(Res.isErr(res));
+    });
+
+    it('converts hex string to uint8arrays', function () {
+        const key = '0x02bcfc5bbf82f1f49630de86a4c19c0b46a0ba3eaf163a9f347c2a1fc526435226';
+
+        const keyArr1 = utils.hexStringToUint8Array(key);
+        const keyArr2 = ethersUtils.arrayify(key);
+
+        expect(keyArr1).toEqual(keyArr2);
     });
 });
--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -43,6 +43,24 @@ export function isValidURL(url: string) {
     }
 }
 
+export function hexStringToUint8Array(hexString: string) {
+    // Remove the '0x' prefix if it exists
+    hexString = hexString.startsWith('0x') ? hexString.slice(2) : hexString;
+
+    // Check if the hex string has an odd length, and pad with a leading zero if needed
+    if (hexString.length % 2 !== 0) {
+        hexString = '0' + hexString;
+    }
+
+    // Create a Uint8Array by iterating through the hex string
+    const uint8Array = new Uint8Array(hexString.length / 2);
+    for (let i = 0; i < hexString.length; i += 2) {
+        uint8Array[i / 2] = parseInt(hexString.substr(i, 2), 16);
+    }
+
+    return uint8Array;
+}
+
 export function logger(namespaces: string[]) {
     namespaces.unshift('rpch');
     const ns = namespaces.join(':');
