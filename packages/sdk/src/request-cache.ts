7eeaa9ab390991aebe497f700111c757ed12cc22
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-04-02 17:16:19 +0200

    improve module load performance

--- packages/sdk/src/request-cache.ts
+++ packages/sdk/src/request-cache.ts
@@ -60,9 +60,13 @@ export function remove(cache: Cache, id: string) {
 /**
  * Generate a sufficiently unique request id.
  */
+let fallbackUUID: () => string;
 export function generateId(_cache: Cache): string {
     if (crypto && 'randomUUID' in crypto) {
         return crypto.randomUUID();
+    }
+    if (!fallbackUUID) {
+        fallbackUUID = genFallbackUUID();
     }
     return fallbackUUID();
 }
@@ -73,35 +77,37 @@ export function generateId(_cache: Cache): string {
  * @license MIT license
  * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
  **/
-const lut: string[] = [];
-for (let i = 0; i < 256; i++) {
-    lut[i] = (i < 16 ? '0' : '') + i.toString(16);
-}
-function fallbackUUID() {
-    const d0 = (Math.random() * 0xffffffff) | 0;
-    const d1 = (Math.random() * 0xffffffff) | 0;
-    const d2 = (Math.random() * 0xffffffff) | 0;
-    const d3 = (Math.random() * 0xffffffff) | 0;
-    return (
-        lut[d0 & 0xff] +
-        lut[(d0 >> 8) & 0xff] +
-        lut[(d0 >> 16) & 0xff] +
-        lut[(d0 >> 24) & 0xff] +
-        '-' +
-        lut[d1 & 0xff] +
-        lut[(d1 >> 8) & 0xff] +
-        '-' +
-        lut[((d1 >> 16) & 0x0f) | 0x40] +
-        lut[(d1 >> 24) & 0xff] +
-        '-' +
-        lut[(d2 & 0x3f) | 0x80] +
-        lut[(d2 >> 8) & 0xff] +
-        '-' +
-        lut[(d2 >> 16) & 0xff] +
-        lut[(d2 >> 24) & 0xff] +
-        lut[d3 & 0xff] +
-        lut[(d3 >> 8) & 0xff] +
-        lut[(d3 >> 16) & 0xff] +
-        lut[(d3 >> 24) & 0xff]
-    );
+function genFallbackUUID() {
+    const lut: string[] = [];
+    for (let i = 0; i < 256; i++) {
+        lut[i] = (i < 16 ? '0' : '') + i.toString(16);
+    }
+    return function () {
+        const d0 = (Math.random() * 0xffffffff) | 0;
+        const d1 = (Math.random() * 0xffffffff) | 0;
+        const d2 = (Math.random() * 0xffffffff) | 0;
+        const d3 = (Math.random() * 0xffffffff) | 0;
+        return (
+            lut[d0 & 0xff] +
+            lut[(d0 >> 8) & 0xff] +
+            lut[(d0 >> 16) & 0xff] +
+            lut[(d0 >> 24) & 0xff] +
+            '-' +
+            lut[d1 & 0xff] +
+            lut[(d1 >> 8) & 0xff] +
+            '-' +
+            lut[((d1 >> 16) & 0x0f) | 0x40] +
+            lut[(d1 >> 24) & 0xff] +
+            '-' +
+            lut[(d2 & 0x3f) | 0x80] +
+            lut[(d2 >> 8) & 0xff] +
+            '-' +
+            lut[(d2 >> 16) & 0xff] +
+            lut[(d2 >> 24) & 0xff] +
+            lut[d3 & 0xff] +
+            lut[(d3 >> 8) & 0xff] +
+            lut[(d3 >> 16) & 0xff] +
+            lut[(d3 >> 24) & 0xff]
+        );
+    };
 }
