5ab287187a8364fcdfd55591c2665eecd9a2b8f4
Ronny Esterluss <esterlus@users.noreply.github.com>
2024-04-24 13:00:20 +0200

    fix offline node handling in Availability Monitor (#636)
    * do not crash on peer failures
    
    * determine offline/online state in peerscache
    
    * halfway there
    
    * should handle offline nodes now
    
    * consolidated log output
    
    * added changeset
    
    * fix run sequence

--- apps/availability-monitor/src/peers-cache.ts
+++ apps/availability-monitor/src/peers-cache.ts
@@ -1,30 +1,53 @@
 import { NodeAPI } from '@rpch/sdk';
 import type { RegisteredNode } from './query';
 
-export type PeersCache = Map<string, Map<string, NodeAPI.Peer>>; // node id -> peer id -> Peer
+export enum NodeStatus {
+    Offline,
+    Online,
+}
 
-export function fetchPeers(
-    cache: PeersCache,
-    node: RegisteredNode,
-): Promise<Map<string, NodeAPI.Peer>> {
-    return new Promise((resolve, reject) => {
-        if (cache.has(node.id)) {
-            return resolve(cache.get(node.id)!);
-        }
-        NodeAPI.getPeers({
-            apiEndpoint: new URL(node.hoprd_api_endpoint),
-            accessToken: node.hoprd_api_token,
-        })
-            .then((res: NodeAPI.Peers | NodeAPI.NodeError) => {
-                if (NodeAPI.isError(res)) {
-                    return reject(res);
-                }
-                const peersMap = new Map(res.connected.map((p) => [p.peerId, p]));
-                cache.set(node.id, peersMap);
-                return resolve(peersMap);
-            })
-            .catch((err) => {
-                return reject(err);
-            });
+export type NodeOffline = { status: NodeStatus.Offline };
+export type NodeOnlinePeers = { status: NodeStatus.Online; peers: Map<string, NodeAPI.Peer> };
+
+export type NodePeers = NodeOnlinePeers | NodeOffline;
+
+export type PeersCache = Map<string, NodePeers>;
+
+export function init(): PeersCache {
+    return new Map();
+}
+
+export async function fetchPeers(cache: PeersCache, node: RegisteredNode): Promise<NodePeers> {
+    // cache hit
+    if (cache.has(node.id)) {
+        return cache.get(node.id)!;
+    }
+
+    // query peers
+    const res: NodeAPI.Peers | NodeAPI.NodeError = await NodeAPI.getPeers({
+        apiEndpoint: new URL(node.hoprd_api_endpoint),
+        accessToken: node.hoprd_api_token,
+    }).catch((err) => {
+        cache.set(node.id, { status: NodeStatus.Offline });
+        throw err;
     });
+
+    if (!res) {
+        cache.set(node.id, { status: NodeStatus.Offline });
+        throw new Error('no results');
+    }
+
+    if (NodeAPI.isError(res)) {
+        cache.set(node.id, { status: NodeStatus.Offline });
+        throw new Error(`${res.status}: ${res.error}`);
+    }
+
+    const peers = new Map(res.connected.map((p) => [p.peerId, p]));
+    const entry = { status: NodeStatus.Online, peers };
+    cache.set(node.id, entry);
+    return entry;
+}
+
+export function isOnline(np: void | NodePeers): np is NodeOnlinePeers {
+    return !!np && np.status === NodeStatus.Online;
 }
