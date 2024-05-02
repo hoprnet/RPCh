35765d99a14e13090f7dbcd48ba7f6734b78d878
ausias-armesto <ausiasarmesto@gmail.com>
2024-05-02 13:00:19 +0200

    Automate load tetsting

--- devkit/loadtesting-k6/src/rpc-server/index.ts
+++ devkit/loadtesting-k6/src/rpc-server/index.ts
@@ -5,19 +5,19 @@ import { JsonRpcMethodCounters } from './wallet/mock-wallet.js';
 import { buildWallet } from './wallet/index.js';
 
 // get values from env
-const url = __ENV.RPC_SERVER_URL;
-const testType = __ENV.TEST_TYPE as OptionTypes;
-const walletType = __ENV.WALLET_TYPE as WalletTypes;
+const url = __ENV.RPC_SERVER_URL || 'http://localhost:45750';
+const testType = __ENV.TEST_TYPE || 'SMOKE';
+const walletType = __ENV.WALLET_TYPE || 'METAMASK';
 
 // get the test option
 console.log(`Starting TestType '${testType}' for wallet type '${walletType}' against '${url}'`);
-export const options = getOption(testType);
+export const options = getOption(testType as OptionTypes);
 
 // create metrics counter
 const counters: JsonRpcMethodCounters = instantiatePayloadCounters();
 
 // Simulated user behavior
 export default function (): void {
-    const wallet = buildWallet(walletType, url);
+    const wallet = buildWallet(walletType as WalletTypes, url);
     wallet.sendRpcCalls(counters);
 }
