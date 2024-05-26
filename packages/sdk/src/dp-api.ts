208f2373c18ec40eb4a450acd4a9e13f640151cf
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-05-26 09:17:38 +0200

    only fetch versions from DP

--- packages/sdk/src/dp-api.ts
+++ packages/sdk/src/dp-api.ts
@@ -1,23 +1,10 @@
-import type { EntryNode } from './entry-node';
-import type { ExitNode } from './exit-node';
-
-export const NoMoreNodes = 'no more nodes';
-export const Unauthorized = 'unauthorized';
-
 /**
  * This module contains all communication with the discovery platform.
- * All calls are behind exponential backoff to avoid DOSing the DP on errors.
  */
 
-export type ClientOps = {
+export type Conn = {
     discoveryPlatformEndpoint: string;
     clientId: string;
-    forceZeroHop: boolean;
-};
-
-export type NodeOps = {
-    discoveryPlatformEndpoint: string;
-    nodeAccessToken: string;
 };
 
 export type Versions = {
@@ -25,67 +12,20 @@ export type Versions = {
     rpcServer: string;
 };
 
-export type Nodes = {
-    entryNodes: EntryNode[];
-    exitNodes: ExitNode[];
-    matchedAt: string;
-    versions: Versions;
-};
-
-export type QuotaParams = {
-    clientId: string;
-    rpcMethod?: string;
-    segmentCount: number;
-    lastSegmentLength?: number;
-    type: 'request' | 'response';
-};
-
-export function fetchNodes(ops: ClientOps, amount: number): Promise<Nodes> {
-    const url = new URL('/api/v1/nodes/pairings', ops.discoveryPlatformEndpoint);
-    url.searchParams.set('amount', `${amount}`);
-    url.searchParams.set('force_zero_hop', `${ops.forceZeroHop}`);
+export async function fetchVersions(conn: Conn): Promise<Versions> {
+    const url = new URL('/api/v1/configs', conn.discoveryPlatformEndpoint);
+    url.searchParams.set('key', 'RPCh_SDK_VERSION');
+    url.searchParams.append('key', 'RPCh_RPC_SERVER_VERSION');
     const headers = {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-rpch-client': ops.clientId,
+        'x-rpch-client': conn.clientId,
     };
-
-    return fetch(url, { headers }).then((res) => {
-        switch (res.status) {
-            case 204: // none found
-                throw new Error(NoMoreNodes);
-            case 403: // unauthorized
-                throw new Error(Unauthorized);
-            default:
-                return res.json();
-        }
-    });
-}
-
-export function fetchQuota(
-    ops: NodeOps,
-    { clientId, segmentCount, rpcMethod, type, lastSegmentLength }: QuotaParams,
-): Promise<void> {
-    const url = new URL(`/api/v1/quota/${type}`, ops.discoveryPlatformEndpoint);
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-rpch-node': ops.nodeAccessToken,
-    };
-    const body = JSON.stringify({
-        clientId,
-        segmentCount,
-        rpcMethod,
-        lastSegmentLength,
-    });
-    return new Promise((pRes, pRej) => {
-        fetch(url, { headers, method: 'POST', body })
-            .then((res) => {
-                if (res.status === 204) {
-                    return pRes();
-                }
-                return pRej(`Unexpected response code: ${res.status}`);
-            })
-            .catch((err) => pRej(err));
-    });
+    const res = await fetch(url, { headers });
+    if (res.ok) {
+        const { RPCh_RPC_SERVER_VERSION: rpcServer, RPCh_SDK_VERSION: sdk } =
+            (await res.json()) as { RPCh_RPC_SERVER_VERSION: string; RPCh_SDK_VERSION: string };
+        return { sdk, rpcServer };
+    }
+    const reason = await res.text();
+    throw new Error(`Error fetching configs [${res.status}]: ${reason}`);
 }
