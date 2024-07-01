3a5cc652720f3d913f5e9e80c84c17015777c5ff
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-07-01 14:47:08 +0200

    added associated nodes migrations

---
+++ apps/discovery-platform/migrations/1718178261909_create-associated-notes.js
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) =>
+    pgm.createTable('associated_nodes', {
+        user_id: { type: 'uuid', references: 'users', notNull: 'true' },
+        node_id: { type: 'varchar(255)', references: 'registered_nodes', notNull: 'true' },
+        created_at: 'createdAt',
+    });
